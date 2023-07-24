84cbbacd344a78cdc5fc491419bb7d7e0b40ed34
Roman Vashurin <rvashurin@gmail.com>
2023-07-24 17:07:18 +0400

    Remove duplicate method for ensemble generation

--- utils/ensemble_generator.py
+++ utils/ensemble_generator.py
@@ -1156,56 +1156,12 @@ class EnsembleGenerator(T5ForConditionalGeneration):
             if model_kwargs.get("encoder_outputs") is None:
                 raise ValueError("If `is_encoder_decoder` is True, make sure that `encoder_outputs` is defined.")
             encoder_outputs_expanded = []
-            for output in enumerate(model_kwargs["encoder_outputs"]):
+            for output in model_kwargs["encoder_outputs"]:
                 encoder_outputs_expanded.append(
                     _expand_dict_for_generation(output)
                 )
             model_kwargs["encoder_outputs"] = encoder_outputs_expanded
             
-        return input_ids, model_kwargs
-
-    @staticmethod
-    def _expand_inputs_for_generation(
-        input_ids: torch.LongTensor,
-        expand_size: int = 1,
-        is_encoder_decoder: bool = False,
-        attention_mask: Optional[torch.LongTensor] = None,
-        encoder_outputs: Optional[ModelOutput] = None,
-        **model_kwargs,
-    ) -> Tuple[torch.LongTensor, Dict[str, Any]]:
-        expanded_return_idx = (
-            torch.arange(input_ids.shape[0])
-            .view(-1, 1)
-            .repeat(1, expand_size)
-            .view(-1)
-            .to(input_ids.device)
-        )
-        input_ids = input_ids.index_select(0, expanded_return_idx)
-
-        if "token_type_ids" in model_kwargs:
-            token_type_ids = model_kwargs["token_type_ids"]
-            model_kwargs["token_type_ids"] = token_type_ids.index_select(
-                0, expanded_return_idx
-            )
-
-        if attention_mask is not None:
-            model_kwargs["attention_mask"] = attention_mask.index_select(
-                0, expanded_return_idx
-            )
-
-        if is_encoder_decoder:
-            if encoder_outputs is None:
-                raise ValueError(
-                    "If `is_encoder_decoder` is True, make sure that `encoder_outputs` is defined."
-                )
-            # The only changed line
-            for i, output in enumerate(encoder_outputs):
-                encoder_outputs[i][
-                    "last_hidden_state"
-                ] = output.last_hidden_state.index_select(
-                    0, expanded_return_idx.to(output.last_hidden_state.device)
-                )
-            model_kwargs["encoder_outputs"] = encoder_outputs
         return input_ids, model_kwargs
 
 
