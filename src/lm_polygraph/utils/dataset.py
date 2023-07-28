537c969962f2e11d642007e89180fbfd930695aa
akim.tsvigun <akimtsvigun@gmail.com>
2023-07-28 14:13:23 +0200

    dataset loading fixed

--- src/lm_polygraph/utils/dataset.py
+++ src/lm_polygraph/utils/dataset.py
@@ -64,6 +64,9 @@ class Dataset:
             dataset = load_dataset(csv_path, "rc.nocontext")[subset]
         elif "babi_qa" in csv_path.lower():
             dataset = load_dataset(csv_path, "en-10k-qa1")[subset]
+        elif "wmt" in csv_path.lower():
+            subset = "de-en" if "de" in [x_column, y_column] else "fr-en"
+            dataset = load_dataset(csv_path, subset)[subset]
         else:
             dataset = load_dataset(csv_path)[subset]
         # In this case this is a NMT dataset
