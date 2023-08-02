15b7cda6f691e19bf72f2333428f9f213a5d6046
Sergey Petrakov <sergeypetrakof@gmail.com>
2023-08-02 22:15:03 +0300

    fix comments to functions and rename parameter

--- src/lm_polygraph/estimators/deg_mat.py
+++ src/lm_polygraph/estimators/deg_mat.py
@@ -14,7 +14,7 @@ class DegMat(Estimator):
     def __init__(
             self,
             similarity_score: Literal["NLI_score", "Jaccard_score"] = "NLI_score",
-            order: Literal["entail", "contra"] = "entail",  # relevant for NLI score case
+            affinity: Literal["entail", "contra"] = "entail",  # relevant for NLI score case
             batch_size: int = 10,
             verbose: bool = False,
             epsilon: float = 1e-13
@@ -26,27 +26,27 @@ class DegMat(Estimator):
             similarity_score (str): The argument to be processed. Possible values are:
                 - 'NLI_score': As a similarity score NLI score is used.
                 - 'Jaccard_score': As a similarity Jaccard score between responces is used.
-            order (str): The argument to be processed. Possible values are. Relevant for the case of NLI similarity score:
-                - 'forward': Compute entailment probability between response_1 and response_2 as p(response_1 -> response_2).
-                - 'backward': Compute entailment probability between response_1 and response_2 as p(response_2 -> response_1).
+            affinity (str): The argument to be processed. Possible values are. Relevant for the case of NLI similarity score:
+                - 'entail': similarity(response_1, response_2) = p_entail(response_1, response_2)
+                - 'contra': similarity(response_1, response_2) = 1 - p_contra(response_1, response_2)
         """
         super().__init__(['blackbox_sample_texts'], 'sequence')
         self.similarity_score = similarity_score
         self.batch_size = batch_size
         if self.similarity_score == "NLI_score":
             DEBERTA.setup()
-        self.order = order
+        self.affinity = affinity
         self.verbose = verbose
         self.epsilon = epsilon
 
     def __str__(self):
         if self.similarity_score == 'NLI_score':
-            return f'DegMat_{self.similarity_score}_{self.order}'
+            return f'DegMat_{self.similarity_score}_{self.affinity}'
         return f'DegMat_{self.similarity_score}'
 
     def U_DegMat(self, answers):
         # The Degree Matrix
-        W = compute_sim_score(answers, self.order, self.epsilon, self.similarity_score)
+        W = compute_sim_score(answers, self.affinity, self.epsilon, self.similarity_score)
         D = np.diag(W.sum(axis=1))
         return np.trace(len(answers) - D) / (len(answers) ** 2)
 
