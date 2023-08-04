d936a23878cf537e26f8050a26c70de5315b13e2
cant-access-rediska0123 <86601618+cant-access-rediska0123@users.noreply.github.com>
2023-08-04 16:34:50 +0300

    Update model.py

--- src/lm_polygraph/utils/model.py
+++ src/lm_polygraph/utils/model.py
@@ -34,7 +34,6 @@ class BlackboxModel(Model):
         openai.api_key = openai_api_key
 
     def generate_texts(self, input_texts: List[str], **args) -> List[str]:
-        print('gen')
         if any(args.get(arg, False) for arg in ['output_scores', 'output_attentions', 'output_hidden_states']):
             raise Exception("Cannot access logits for blackbox model")
 
