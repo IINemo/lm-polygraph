093f4ca2b0d21f9988e4977d9db852426c4eecab
Roman Vashurin <rvashurin@gmail.com>
2023-08-06 21:38:03 +0400

    Remove caching of norm boundaries

--- src/lm_polygraph/utils/normalize.py
+++ src/lm_polygraph/utils/normalize.py
@@ -16,13 +16,11 @@ DEFAULT_CACHE_PATH = f'{pathlib.Path(__file__).parent.resolve()}/normalization'
 def can_normalize_ue(est: Estimator, model_path: str, cache_path: str = DEFAULT_CACHE_PATH) -> bool:
     archive_path = model_path.split('/')[-1] + '.json'
     filepath = os.path.join(cache_path, archive_path)
-    if not os.path.exists(filepath):
-        sys.stdout.write(f'No cached normalizing bounds for estimator: {str(est)}. Looking in remote storage...')
-        try:
-            wget.download(HOST + '/normalization/' + archive_path, out = filepath)
-        except:
-            sys.stderr.write('Failed, no normalization...')
-            return False
+    try:
+        wget.download(HOST + '/normalization/' + archive_path, out = filepath)
+    except:
+        sys.stderr.write('Failed, no normalization...')
+        return False
     with open(filepath, 'r') as f:
         ue_bounds = json.load(f)
     return str(est) in ue_bounds.keys()
