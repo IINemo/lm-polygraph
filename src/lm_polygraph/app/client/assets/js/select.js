568b9fde0cee6c7859962a54230adee5458d1ba4
kirill.fedyanin <kirill.fedyanin@gmail.com>
2023-08-15 11:54:27 +0400

    Methods as a dict

--- src/lm_polygraph/app/client/assets/js/select.js
+++ src/lm_polygraph/app/client/assets/js/select.js
@@ -11,22 +11,6 @@ function toOptions(list) {
 }
 
 
-Vue.component('treeselect', VueTreeselect.Treeselect);
-const modelVue = new Vue({
-    el: '#model',
-    data: {
-        modelSelected: 'BLOOMz 560M',
-        options: toOptions([
-            'GPT-4', 'GPT-3.5-turbo',
-            'Dolly 3b', 'Dolly 7b', 'Dolly 12b',
-            'BLOOMz 560M', 'BLOOMz 3b', 'BLOOMz 7b', 'Falcon 7b',
-            'Llama 2 7b', 'Llama 2 13b',
-            'Open Llama 3b', 'Open Llama 7b', 'Open Llama 13b',
-            'Flan T5 XL', 'T5 XL NQ', 'BART Large CNN']),
-        value: '',
-    },
-});
-
 
 
 function modelType(newModel) {
@@ -42,6 +26,58 @@ function modelType(newModel) {
     }
     return newType
 }
+
+const typeMethods = {
+    'ensemble': [
+        'EP-S-Total-Uncertainty', 'EP-S-RMI', 'PE-S-Total-Uncertainty',
+        'PE-S-RMI', 'EP-T-Total-Uncertainty', 'EP-T-Data-Uncertainty', 'EP-T-Mutual-Information',
+        'EP-T-RMI', 'EP-T-EPKL', 'EP-T-Entropy-Top5', 'EP-T-Entropy-Top10',
+        'EP-T-Entropy-Top15', 'PE-T-Total-Uncertainty', 'PE-T-Data-Uncertainty',
+        'PE-T-Mutual-Information', 'PE-T-RMI', 'PE-T-EPKL',
+        'PE-T-Entropy-Top5', 'PE-T-Entropy-Top10', 'PE-T-Entropy-Top15'
+    ],
+    'T5': [
+        'Maximum Sequence Probability', 'Perplexity', 'Mean Token Entropy',
+        'Mean Pointwise Mutual Information', 'Mean Conditional Pointwise Mutual Information',
+        'P(True)', 'P(True) Sampling',
+        'Monte Carlo Sequence Entropy', 'Monte Carlo Normalized Sequence Entropy',
+        'Lexical Similarity',
+        "Eigenvalue Laplacian", "Eccentricity", "Degree Matrix", "Number of Semantic Sets",
+        'Semantic Entropy',
+        'Mahalanobis Distance', 'Mahalanobis Distance - Encoder', 'RDE', 'RDE - Encoder',
+        'HUQ - Decoder', 'HUQ - Encoder'
+    ],
+    'openai':
+    [
+        'Lexical Similarity',
+        "Eigenvalue Laplacian", "Eccentricity", "Degree Matrix", "Number of Semantic Sets",
+    ],
+    'seq2seq': [
+        'Maximum Sequence Probability', 'Perplexity', 'Mean Token Entropy',
+        'Mean Pointwise Mutual Information', 'Mean Conditional Pointwise Mutual Information',
+        'P(True)', 'P(True) Sampling',
+        'Monte Carlo Sequence Entropy', 'Monte Carlo Normalized Sequence Entropy',
+        'Lexical Similarity',
+        "Eigenvalue Laplacian", "Eccentricity", "Degree Matrix", "Number of Semantic Sets",
+        'Semantic Entropy', 'Mahalanobis Distance', 'RDE', 'HUQ - Decoder'
+    ]
+}
+
+Vue.component('treeselect', VueTreeselect.Treeselect);
+new Vue({
+    el: '#model',
+    data: {
+        modelSelected: 'BLOOMz 560M',
+        options: toOptions([
+            'GPT-4', 'GPT-3.5-turbo',
+            'Dolly 3b', 'Dolly 7b', 'Dolly 12b',
+            'BLOOMz 560M', 'BLOOMz 3b', 'BLOOMz 7b', 'Falcon 7b',
+            'Llama 2 7b', 'Llama 2 13b',
+            'Open Llama 3b', 'Open Llama 7b', 'Open Llama 13b',
+            'Flan T5 XL', 'T5 XL NQ', 'BART Large CNN', 'Ensemble']),
+        value: '',
+    },
+});
 
 
 Vue.component('treeselect', VueTreeselect.Treeselect);
@@ -62,37 +98,8 @@ new Vue({
             }
             this.model = newModel;
             this.type = newType;
-            if (newType === 'ensemble') {
-                return toOptions([
-                    'EP-S-Total-Uncertainty', 'EP-S-RMI', 'PE-S-Total-Uncertainty',
-                    'PE-S-RMI', 'EP-T-Total-Uncertainty', 'EP-T-Data-Uncertainty', 'EP-T-Mutual-Information',
-                    'EP-T-RMI', 'EP-T-EPKL', 'EP-T-Entropy-Top5', 'EP-T-Entropy-Top10',
-                    'EP-T-Entropy-Top15', 'PE-T-Total-Uncertainty', 'PE-T-Data-Uncertainty',
-                    'PE-T-Mutual-Information', 'PE-T-RMI', 'PE-T-EPKL',
-                    'PE-T-Entropy-Top5', 'PE-T-Entropy-Top10', 'PE-T-Entropy-Top15']);
-            } else if (newType === 'T5') {
-                return toOptions(['Maximum Sequence Probability', 'Perplexity', 'Mean Token Entropy',
-                    'Mean Pointwise Mutual Information', 'Mean Conditional Pointwise Mutual Information',
-                    'P(True)', 'P(True) Sampling',
-                    'Monte Carlo Sequence Entropy', 'Monte Carlo Normalized Sequence Entropy',
-                    'Lexical Similarity',
-                    "Eigenvalue Laplacian", "Eccentricity", "Degree Matrix", "Number of Semantic Sets",
-                    'Semantic Entropy',
-                    'Mahalanobis Distance', 'Mahalanobis Distance - Encoder', 'RDE', 'RDE - Encoder',
-                    'HUQ - Decoder', 'HUQ - Encoder']);
-            } else if (newType === 'openai') {
-                return toOptions([
-                    'Lexical Similarity',
-                    "Eigenvalue Laplacian", "Eccentricity", "Degree Matrix", "Number of Semantic Sets",]);
-            } else {
-                return toOptions(['Maximum Sequence Probability', 'Perplexity', 'Mean Token Entropy',
-                    'Mean Pointwise Mutual Information', 'Mean Conditional Pointwise Mutual Information',
-                    'P(True)', 'P(True) Sampling',
-                    'Monte Carlo Sequence Entropy', 'Monte Carlo Normalized Sequence Entropy',
-                    'Lexical Similarity',
-                    "Eigenvalue Laplacian", "Eccentricity", "Degree Matrix", "Number of Semantic Sets",
-                    'Semantic Entropy', 'Mahalanobis Distance', 'RDE', 'HUQ - Decoder']);
-            }
+
+            return toOptions(typeMethods[this.type])
         }
     }
 });
