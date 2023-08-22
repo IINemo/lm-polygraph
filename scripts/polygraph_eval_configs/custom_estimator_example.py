451bdbed96331852d26a49ce5f44c46c14c5f3a6
Daniil Vasilev <steam.haver@gmail.com>
2023-08-22 09:13:23 +0000

    fix

--- scripts/polygraph_eval_configs/custom_estimator_example.py
+++ scripts/polygraph_eval_configs/custom_estimator_example.py
@@ -5,14 +5,14 @@ from typing import Dict
 from lm_polygraph.estimators.estimator import Estimator
 
 
-# This estimator is essentialy MSP * 2
+# This estimator is essentialy -MSP
 class CustomEstimatorExample(Estimator):
     def __init__(self):
         super().__init__(['greedy_log_likelihoods'], 'sequence')
 
     def __str__(self):
-        return 'DoubleMaximumSequenceProbability'
+        return 'CustomEstimatorExample'
 
     def __call__(self, stats: Dict[str, np.ndarray]) -> np.ndarray:
         log_likelihoods = stats['greedy_log_likelihoods']
-        return np.array([-2 * np.sum(l) for l in log_likelihoods])
+        return np.array([np.sum(l) for l in log_likelihoods])
