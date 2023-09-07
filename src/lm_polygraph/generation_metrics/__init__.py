4558db2398b71efcd0b911f732f66db343b57a93
Artem Vazhentsev <vazhentsev@airi.net>
2023-09-07 19:23:17 +0300

    added accuracy

--- src/lm_polygraph/generation_metrics/__init__.py
+++ src/lm_polygraph/generation_metrics/__init__.py
@@ -2,3 +2,4 @@ from .rouge import RougeMetric
 from .wer_tokenwise import WERTokenwiseMetric
 from .model_score import ModelScoreSeqMetric, ModelScoreTokenwiseMetric
 from .bart_score import BartScoreSeqMetric
+from .accuracy import AccuracyMetric
