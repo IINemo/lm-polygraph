45fd3b7d601b973640d57d02d9fe022b27d38788
SpeedOfMagic <vasilev.daniil18@gmail.com>
2023-09-08 20:10:29 +0300

    black + add to req

--- test/test_lm_polygraph.py
+++ test/test_lm_polygraph.py
@@ -9,12 +9,18 @@ def exec_bash(s):
 def test_just_works_cli():
     command = 'scripts/polygraph_eval --estimator LexicalSimilarity --estimator_kwargs \'{"metric": "rouge2"}\' --dataset SpeedOfMagic/trivia_qa_tiny --model bigscience/bloomz-560m --no-ignore_exceptions --subsample_eval_dataset 10'
     exec_result = exec_bash(command)
-    assert exec_result.returncode == 0, f'polygraph_eval returned code {exec_result.returncode} != 0'
-    os.remove('_seed1')
+    assert (
+        exec_result.returncode == 0
+    ), f"polygraph_eval returned code {exec_result.returncode} != 0"
+    os.remove("_seed1")
 
 
 def test_just_works_hydra():
-    command = 'HYDRA_CONFIG=../test/configs/test_polygraph_eval.yaml scripts/polygraph_eval'
+    command = (
+        "HYDRA_CONFIG=../test/configs/test_polygraph_eval.yaml scripts/polygraph_eval"
+    )
     exec_result = exec_bash(command)
-    assert exec_result.returncode == 0, f'polygraph_eval returned code {exec_result.returncode} != 0'
-    os.remove('_seed1')
+    assert (
+        exec_result.returncode == 0
+    ), f"polygraph_eval returned code {exec_result.returncode} != 0"
+    os.remove("_seed1")
