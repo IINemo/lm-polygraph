cd90c0ac3325356b9d04b88b03a3517deadebc51
Roman Vashurin <rvashurin@gmail.com>
2023-09-25 12:27:41 +0300

    Remove breakpoint, remove cli args test

--- test/test_lm_polygraph.py
+++ test/test_lm_polygraph.py
@@ -6,15 +6,6 @@ def exec_bash(s):
     return subprocess.run(s, shell=True)
 
 
-def test_just_works_cli():
-    command = 'scripts/polygraph_eval --estimator LexicalSimilarity --estimator_kwargs \'{"metric": "rouge2"}\' --dataset SpeedOfMagic/trivia_qa_tiny --model bigscience/bloomz-560m --no-ignore_exceptions --subsample_eval_dataset 10 --no-use_seq_ue'
-    exec_result = exec_bash(command)
-    assert (
-        exec_result.returncode == 0
-    ), f"polygraph_eval returned code {exec_result.returncode} != 0"
-    os.remove("ue_manager_seed1")
-
-
 def test_just_works_hydra():
     command = (
         "HYDRA_CONFIG=../test/configs/test_polygraph_eval.yaml scripts/polygraph_eval"
