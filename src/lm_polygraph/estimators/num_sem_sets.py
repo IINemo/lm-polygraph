f6e30dd1d38dd8187cb6982fd099c0b82791f424
Sergey Petrakov <sergeypetrakof@gmail.com>
2023-10-02 22:08:26 +0300

    fixed Num Semantic Sets

--- src/lm_polygraph/estimators/num_sem_sets.py
+++ src/lm_polygraph/estimators/num_sem_sets.py
@@ -3,17 +3,45 @@ import numpy as np
 from typing import Dict, Literal
 
 from .estimator import Estimator
-from .common import DEBERTA
+from .common import DEBERTA, compute_sim_score
 import torch.nn as nn
 
+
 softmax = nn.Softmax(dim=1)
+
+def _get_pairs(lst):
+        pairs = []
+        for i in range(len(lst)):
+            for j in range(i + 1, len(lst)):
+                pairs.append((lst[i], lst[j], i, j))
+        return pairs
+
+def find_connected_components(graph):
+    def dfs(node, component):
+        visited[node] = True
+        component.append(node)
+
+        for neighbor in graph[node]:
+            if not visited[neighbor]:
+                dfs(neighbor, component)
+
+    visited = [False] * len(graph)
+    components = []
+
+    for i in range(len(graph)):
+        if not visited[i]:
+            component = []
+            dfs(i, component)
+            components.append(component)
+
+    return components
 
 
 class NumSemSets(Estimator):
     def __init__(
             self,
             batch_size: int = 10,
-            verbose: bool = False,
+            verbose: bool = False
     ):
         """
         A number of semantic sets in response (higher = bigger uncertainty).
@@ -23,6 +51,7 @@ class NumSemSets(Estimator):
         self.batch_size = batch_size
         DEBERTA.setup()
         self.verbose = verbose
+        self.device = DEBERTA.device 
 
     def __str__(self):
         return f'NumSemSets'
@@ -32,37 +61,80 @@ class NumSemSets(Estimator):
         for i in range(len(lst) - 1):
             pairs.append((lst[i], lst[i + 1]))
         return pairs
-
+    
     def U_NumSemSets(self, answers):
 
-        lst = self.get_pairs_semsets(answers)
-        # basically we have only 1 semantic set
-        num_sets = 1
-        device = DEBERTA.deberta.device
-        # we iterate over responces and incerase num_sets if the NLI condition is fulfilled
-        for (sentence_1, sentence_2) in lst:
-            # Tokenize input sentences
-            encoded_input_forward = DEBERTA.deberta_tokenizer(sentence_1, sentence_2, return_tensors='pt').to(device)
-            encoded_input_backward = DEBERTA.deberta_tokenizer(sentence_2, sentence_1, return_tensors='pt').to(device)
+        num_sets = len(answers)
 
-            logits_forward = DEBERTA.deberta(**encoded_input_forward).logits.detach().to(device)
-            logits_backward = DEBERTA.deberta(**encoded_input_backward).logits.detach().to(device)
+        W_entail_forward = np.eye(len(answers))
+        W_entail_backward = np.eye(len(answers))
+        W_contra_forward = np.eye(len(answers))
+        W_contra_backward = np.eye(len(answers))
 
-            probs_forward = softmax(logits_forward).to(device)
-            probs_backward = softmax(logits_backward).to(device)
+        pairs = _get_pairs(answers)
+        device = DEBERTA.device 
 
-            p_entail_forward = probs_forward[0][2]
-            p_entail_backward = probs_backward[0][2]
+        softmax = nn.Softmax(dim=1)
 
-            p_contra_forward = probs_forward[0][0]
-            p_contra_backward = probs_backward[0][0]
+        forward_texts = [pair[:2] for pair in pairs]
+        backward_texts = [pair[:2][::-1] for pair in pairs]
 
-            if (p_entail_forward > p_contra_forward) & (p_entail_backward > p_contra_backward):
-                pass
-            else:
-                num_sets += 1
+        encoded_forward_batch = DEBERTA.deberta_tokenizer.batch_encode_plus(forward_texts, padding=True, return_tensors='pt').to(device)
+        encoded_backward_batch = DEBERTA.deberta_tokenizer.batch_encode_plus(backward_texts, padding=True, return_tensors='pt').to(device)
 
-        return num_sets
+        logits_forward_batch = DEBERTA.deberta(**encoded_forward_batch).logits.detach().to(device)
+        logits_backward_batch = DEBERTA.deberta(**encoded_backward_batch).logits.detach().to(device)
+
+        probs_forward_batch = softmax(logits_forward_batch)
+        probs_backward_batch = softmax(logits_backward_batch)
+
+        for n, (sentence_1, sentence_2, i, j) in enumerate(pairs):
+            probs_forward = probs_forward_batch[n].unsqueeze(0)
+            probs_backward = probs_backward_batch[n].unsqueeze(0)
+
+            a_nli_entail_forward = probs_forward[0][2]
+            a_nli_entail_backward = probs_backward[0][2]
+
+            a_nli_contra_forward = probs_forward[0][0]
+            a_nli_contra_backward = probs_backward[0][0]
+            
+            forward_entail_w = a_nli_entail_forward.item()
+            backward_entail_w = a_nli_entail_backward.item()
+
+            forward_contra_w = a_nli_contra_forward.item()
+            backward_contra_w = a_nli_contra_backward.item()
+
+            W_entail_forward[i, j] = forward_entail_w
+            W_entail_backward[i, j] = backward_entail_w
+            W_contra_forward[i, j] = forward_contra_w
+            W_contra_backward[i, j] = backward_contra_w
+
+        W = (W_entail_forward > W_contra_forward).astype(int)+(W_entail_backward > W_contra_backward).astype(int)
+
+        a = [[i] for i in range(W.shape[0])]
+
+        # Iterate through each row in 'W' and update the corresponding row in 'a'
+        for i, row in enumerate(W):
+            # Find the indices of non-zero elements in the current row
+            non_zero_indices = np.where(row != 0)[0]
+            
+            # Append the non-zero indices to the corresponding row in 'a'
+            a[i].extend(non_zero_indices.tolist())
+
+        # Create an adjacency list representation of the graph
+        graph = [[] for _ in range(len(a))]
+        for sublist in a:
+            for i in range(len(sublist) - 1):
+                graph[sublist[i]].append(sublist[i + 1])
+                graph[sublist[i + 1]].append(sublist[i])
+
+        # Find the connected components
+        connected_components = find_connected_components(graph)
+
+        # Calculate the number of connected components
+        num_components = len(connected_components)
+        
+        return num_components
 
     def __call__(self, stats: Dict[str, np.ndarray]) -> np.ndarray:
         res = []
