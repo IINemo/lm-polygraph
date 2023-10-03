8f3b1d160e82824700433c8ec2cb4caeaed989ee
Roman Vashurin <rvashurin@gmail.com>
2023-10-03 12:56:52 +0300

    Adapt eig val lap to common semantic matrix

--- src/lm_polygraph/estimators/eig_val_laplacian.py
+++ src/lm_polygraph/estimators/eig_val_laplacian.py
@@ -28,11 +28,17 @@ class EigValLaplacian(Estimator):
                 - 'entail': similarity(response_1, response_2) = p_entail(response_1, response_2)
                 - 'contra': similarity(response_1, response_2) = 1 - p_contra(response_1, response_2)
         """
-        super().__init__(['blackbox_sample_texts'], 'sequence')
+        if self.similarity_score == 'NLI_score':
+            DEBERTA.setup()
+            if self.affinity == 'entail':
+                super().__init__(['semantic_matrix_entail',
+                                  'blackbox_sample_texts'], 'sequence')
+            else:
+                super().__init__(['semantic_matrix_contra',
+                                  'blackbox_sample_texts'], 'sequence')
+
         self.similarity_score = similarity_score
         self.batch_size = batch_size
-        if self.similarity_score == "NLI_score":
-            DEBERTA.setup()
         self.affinity = affinity
         self.verbose = verbose
         self.device = DEBERTA.device
@@ -42,8 +48,18 @@ class EigValLaplacian(Estimator):
             return f'EigValLaplacian_{self.similarity_score}_{self.affinity}'
         return f'EigValLaplacian_{self.similarity_score}'
 
-    def U_EigVal_Laplacian(self, answers):
-        W = compute_sim_score(answers = answers, affinity = self.affinity, similarity_score = self.similarity_score)
+    def U_EigVal_Laplacian(self, i, stats):
+        answers = stats['blackbox_sample_texts']
+
+        if self.similarity_score == 'NLI_score':
+            if self.affinity == 'entail':
+                W = stats['semantic_matrix_entail'][i, :, :]
+            else:
+                W = 1 - stats['semantic_matrix_contra'][i, :, :]
+            W = (W + np.transpose(W)) / 2
+        else:
+            W = compute_sim_score(answers = answers, affinity = self.affinity, similarity_score = self.similarity_score)
+
         D = np.diag(W.sum(axis=1))
         D_inverse_sqrt = np.linalg.inv(np.sqrt(D))
         L = np.eye(D.shape[0]) - D_inverse_sqrt @ W @ D_inverse_sqrt
@@ -51,9 +67,9 @@ class EigValLaplacian(Estimator):
 
     def __call__(self, stats: Dict[str, np.ndarray]) -> np.ndarray:
         res = []
-        for answers in stats['blackbox_sample_texts']:
+        for i, answers in enumerate(stats['blackbox_sample_texts']:
             if self.verbose:
                 print(f"generated answers: {answers}")
-            res.append(self.U_EigVal_Laplacian(answers))
+            res.append(self.U_EigVal_Laplacian(i, stats))
         return np.array(res)
     
