207de730a56ed7981d894c8a712376205a415389
Ekaterina Fadeeva <rediska01234@gmail.com>
2023-11-02 00:53:03 +0300

    More documentation: stat_calculators & utils

--- src/lm_polygraph/stat_calculators/prompt.py
+++ src/lm_polygraph/stat_calculators/prompt.py
@@ -8,13 +8,46 @@ from lm_polygraph.utils.model import WhiteboxModel
 
 
 class PromptCalculator(StatCalculator):
+    """
+    Calculates the probability for a specific token to be generated from the specific prompt.
+    Used for P(True)-based methods.
+    """
+
     def __init__(self, prompt: str, expected: str, method: str):
+        """
+        Parameters:
+            prompt (str): Prompt to use for estimating the answer of.
+                The following values can be used in the prompt:
+                - q: input text
+                - a: generation text
+                - s: list of several generation samples.
+                Prompt example: 'Question: {q}. Is the following answer true? {a}'.
+            expected (str): string to measure probability of. Must be decoded into one token,
+                otherwise an exception will be raised.
+            method (str): the name of the statistics to calculate with this calculator.
+        """
         super().__init__([method], ['greedy_texts', 'sample_texts'])
         self.method = method
         self.prompt = prompt
         self.expected = expected
 
-    def __call__(self, dependencies: Dict[str, np.array], texts: List[str], model: WhiteboxModel, max_new_tokens: int = 100, **kwargs) -> Dict[str, np.ndarray]:
+    def __call__(self, dependencies: Dict[str, np.array], texts: List[str], model: WhiteboxModel,
+                 max_new_tokens: int = 100, **kwargs) -> Dict[str, np.ndarray]:
+        """
+        Calculates the probability for `expected` to be generated from `prompt`.
+
+        Parameters:
+            dependencies (Dict[str, np.ndarray]): input statistics, consisting of:
+                - 'greedy_texts' (List[str]): model generations for this batch,
+                - 'sample_texts' (List[List[str]]): several sampling generations for each input text.
+            texts (List[str]): Input texts batch used for model generation.
+            model (Model): Model used for generation.
+            max_new_tokens (int): Maximum number of new tokens at model generation. Default: 100.
+        Returns:
+            Dict[str, np.ndarray]: dictionary with the following items:
+                - `method` (List[float]): logarithms of probability of generating `expected` from prompt formatted
+                    at each input text.
+        """
         expected_tokens = model.tokenizer([self.expected])['input_ids'][0]
         expected_tokens = [t for t in expected_tokens
                            if t != model.tokenizer.eos_token_id and t != model.tokenizer.bos_token_id]
--- src/lm_polygraph/stat_calculators/stat_calculator.py
+++ src/lm_polygraph/stat_calculators/stat_calculator.py
@@ -9,24 +9,70 @@ STAT_DEPENDENCIES: Dict[str, List[str]] = {}
 
 
 class StatCalculator(ABC):
+    """
+    Abstract class for some particular statistics calculation. Used to re-use same statistics across different
+    uncertainty estimators at `lm_polygraph.estimators`. See the list of available calculators at
+    lm_polygraph/stat_calculators/__init__.py.
+
+    While estimators specify `stats_dependencies` to re-use these StatCalculator calculations, calculators can
+    also specify dependencies on other calculators.
+
+    UEManager at lm_polygraph.utils.manager will order all the needed calculators and estimators to be called in
+    the correct order. Any cycle dependencies among calculators will be spotted by UEManager and end with an exception.
+
+    Each new StatCalculator needs to be registered at lm_polygraph/stat_calculators/__init__.py to be seen be UEManager.
+    """
+
     def __init__(self, stats: List[str], stat_dependencies: List[str]):
+        """
+        Parameters:
+            stats: List[str]: Names of statiscits which can be calculated by using this StatCalculator.
+            stat_dependencies: List[str]: Names of statistics which this calculator needs to use. Can be any names of
+                other StatCalculators. Any cycle dependencies among calculators will be spotted by UEManager and
+                end with an exception.
+        """
         self._stats = stats
         self._stat_dependencies = stat_dependencies
 
     @abstractmethod
-    def __call__(self, dependencies: Dict[str, np.array], texts: List[str], model: WhiteboxModel, max_new_tokens: int = 100, **kwargs) -> Dict[str, np.ndarray]:
+    def __call__(self, dependencies: Dict[str, np.array], texts: List[str], model: Model,
+                 max_new_tokens: int = 100, **kwargs) -> Dict[str, np.ndarray]:
+        """
+        Abstract method. Calculates the statistic based on the other provided statistics.
+
+        Parameters:
+            dependencies (Dict[str, np.ndarray]): input statistics, which includes values from
+                statistics calculators for each `stat_dependencies`.
+            texts (List[str]): Input texts batch used for model generation.
+            model (Model): Model used for generation.
+            max_new_tokens (int): Maximum number of new tokens at model generation. Default: 100.
+        Returns:
+            Dict[str, np.ndarray]: dictionary with calculated statistics under all keys from `stats`.
+        """
         raise Exception('Not implemented')
 
     @property
     def stats(self) -> List[str]:
+        """
+        Returns:
+            List[str]: Names of statistics which can be calculated by this class.
+        """
         return self._stats
 
     @property
     def stat_dependencies(self) -> List[str]:
+        """
+        Returns:
+            List[str]: Names of statistics dependencies which this class needs at __call__.
+        """
         return self._stat_dependencies
 
 
 def register(calculator_class: StatCalculator):
+    """
+    Registers a new statistics calculator to be seen by UEManager for properly organizing the calculations order.
+    Needs to be called at lm_polygraph/stat_calculators/__init__.py for all stat calculators used in running benchmarks.
+    """
     for stat in calculator_class.stats:
         if stat in STAT_CALCULATORS.keys():
             continue
