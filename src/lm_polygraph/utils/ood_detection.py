583a9f0d95eb21e724807c6c9abc820ac372e6e9
Artem Vazhentsev <vazhentsev@airi.net>
2023-11-22 14:52:54 +0300

    updated ood detection

--- src/lm_polygraph/utils/ood_detection.py
+++ src/lm_polygraph/utils/ood_detection.py
@@ -8,10 +8,11 @@ def calculate_ood_from_mans(manager_id, manager_ood, ood_metrics):
         
     results = {}
     for ood_metric in ood_metrics:
+        results[str(ood_metric)] = {}
         for ue_method in ue_methods:
             ue_id = manager_id.estimations[("sequence", ue_method)]
             ue_ood = manager_ood.estimations[("sequence", ue_method)]
             ood_labels = [0]*len(ue_id) + [1]*len(ue_ood)
             ue = np.concatenate([ue_id, ue_ood])
-            results[(ood_metric, ue_method)] = ood_metric(ue, ood_labels)
+            results[str(ood_metric)][ue_method] = ood_metric(ue, ood_labels)
     return results
