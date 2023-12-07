cc6fa602a84dd4aec9730e281f55771d97418635
akim.tsvigun <akimtsvigun@gmail.com>
2023-12-07 11:25:12 +0100

    bug fixed

--- src/lm_polygraph/stat_calculators/greedy_probs.py
+++ src/lm_polygraph/stat_calculators/greedy_probs.py
@@ -130,7 +130,6 @@ class GreedyProbsCalculator(StatCalculator):
                 num_return_sequences=1,
             )
             logits = torch.stack(out.scores, dim=1)
-            logits = logits.log_softmax(-1)
 
             sequences = out.sequences
             embeddings_encoder, embeddings_decoder = get_embeddings_from_output(
