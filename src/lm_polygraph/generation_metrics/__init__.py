77353ee779b9ced7b7cf1981e833974b98075899
Artem Vazhentsev <vazhentsev@airi.net>
2024-03-15 18:10:58 +0300

    added alignscore

--- src/lm_polygraph/generation_metrics/__init__.py
+++ src/lm_polygraph/generation_metrics/__init__.py
@@ -3,3 +3,4 @@ from .model_score import ModelScoreSeqMetric, ModelScoreTokenwiseMetric
 from .bart_score import BartScoreSeqMetric
 from .accuracy import AccuracyMetric
 from .comet import Comet
+from .alignscore import AlignScore
