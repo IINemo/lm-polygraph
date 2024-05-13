2ec4045d346774f4a149e1f535d7b66de126c3b8
Roman Vashurin <rvashurin@gmail.com>
2024-05-13 15:05:06 +0400

    Add data loading for normalization fitting

---
+++ src/lm_polygraph/utils/common.py
@@ -0,0 +1,6 @@
+from typing import Tuple
+
+def seq_man_key(metric_name: str) -> Tuple[str, str]:
+    """Convert metric name to format of seq-level name format of
+    saved manager archive."""
+    return ('sequence', metric_name)
