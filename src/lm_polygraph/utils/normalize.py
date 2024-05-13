d833775190e3d5a4a92e175a9bbb13c107476ec4
Roman Vashurin <rvashurin@gmail.com>
2024-05-13 15:59:13 +0400

    Add fitting procedure and saving for all normalizers

--- src/lm_polygraph/utils/normalize.py
+++ src/lm_polygraph/utils/normalize.py
@@ -1,10 +1,14 @@
 from typing import List, Tuple, Dict
 
 import numpy as np
+from cir_model import CenteredIsotonicRegression
+from scipy.stats import binned_statistic
+
 from lm_polygraph.utils.manager import UEManager
 from lm_polygraph.utils.common import seq_man_key
 
-def concat_mans_data(mans_data_dicts, names):
+
+def _concat_mans_data(mans_data_dicts, names):
     """Concatenates data from multiple manager data dictionaries.
 
     Args:
@@ -80,11 +84,75 @@ def filter_nans(gen_metrics: np.ndarray, ues: np.ndarray) -> Tuple[np.ndarray, n
     return ues, gen_metrics
 
 
-def fit_isotonic_pcc():
+def fit_isotonic_pcc(gen_metrics: np.ndarray, ues: np.ndarray) -> CenteredIsotonicRegression:
+    """Fits centered isotonic regression to the gen_metrics and ues data."""
+    cir = CenteredIsotonicRegression(out_of_bounds='clip',
+                                     increasing=False,
+                                     y_min=0, y_max=1)
+    cir.fit(ues, gen_metrics)
 
-def fit_binned_pcc():
+    return cir
 
-def fit_quantile():
 
-def fit_linear():
+def _get_bin_indices(ue, num_bins):
+    per_bin = len(ue) / num_bins
 
+    lesser_bin = math.floor(per_bin)
+    greater_bin = math.ceil(per_bin)
+
+    n_lesser_bins = (greater_bin * num_bins) - len(ue)
+    n_greater_bins = num_bins - n_lesser_bins
+    
+    greater_start_index = n_lesser_bins * lesser_bin
+
+    bin_indices = [i * lesser_bin for i in range(n_lesser_bins)] + \
+                  [greater_start_index + i * greater_bin for i in range(n_greater_bins)] + \
+                  [len(ue) - 1]
+
+    return bin_indices
+
+
+def _get_bin_edges(ue, num_bins):
+    sorted_ue = np.sort(ue)
+    bin_indices = _get_bin_indices(sorted_ue, num_bins)
+    bin_edges = np.unique([sorted_ue[i] for i in bin_indices]).tolist()
+
+    return bin_edges
+
+
+def _get_bins(ue, metric, edges):
+    metric_bins = binned_statistic(ue,
+                                   metric,
+                                   bins=edges,
+                                   statistic='mean')
+
+    return metric_bins
+
+
+def fit_binned_pcc(gen_metrics: np.ndarray,
+                   ues: np.ndarray, num_bins: int) -> CenteredIsotonicRegression:
+    bin_edges = _get_bin_edges(ues, num_bins)
+    metric_bins, std_bins, sem_bins = _get_bins(ues, gen_metrics, bin_edges)
+
+    binned_metric = metric_bins.statistic
+
+    return {
+        "binned_metric": binned_metric,
+        "bin_edges": bin_edges
+    }
+
+
+def fit_quantile(ues: np.ndarray) -> QuantileTransformer:
+    """Fits QuantileTransformer to the gen_metrics and ues data."""
+    scaler = QuantileTransformer(output_distribution='uniform')
+    scaler.fit(ues)
+
+    return scaler
+
+
+def fit_min_max(ues: np.ndarray) -> MinMaxScaler:
+    """Fits MinMaxScaler to the gen_metrics and ues data."""
+    scaler = MinMaxScaler(clip=True)
+    scaler.fit(ues)
+
+    return scaler
