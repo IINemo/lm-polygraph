98db12b4e5b502fb6cd592fb7fb9efa128686f62
Roman Vashurin <rvashurin@gmail.com>
2024-05-17 15:27:29 +0400

    Add notebook with normalization experiments, implemet normalizers as separate classes

---
+++ src/lm_polygraph/normalizers/quantile.py
@@ -0,0 +1,34 @@
+# Description: Quantile normalizer for UE values
+
+import pickle
+
+import numpy as np
+from sklearn.preprocessing import QuantileTransformer
+
+from lm_polygraph.normalizers.base import BaseUENormalizer
+
+class QuantileNormalizer(BaseUENormalizer):
+    def __init__(self):
+        self.scaler = None
+
+    def fit(self, ues: np.ndarray) -> None:
+        """Fits QuantileTransformer to the gen_metrics and ues data."""
+        self.scaler = QuantileTransformer(output_distribution='uniform')
+        conf = -ues
+        self.scaler.fit(conf[:, None])
+
+    def transform(self, ues: np.ndarray) -> np.ndarray:
+        """Transforms the ues data using the fitted QuantileTransformer."""
+        conf = -ues
+        return self.scaler.transform(conf[:, None]).squeeze()
+
+    def dumps(self) -> str:
+        """Dumps the QuantileNormalizer object to a string."""
+        return pickle.dumps(self.scaler)
+
+    @staticmethod
+    def loads(scaler):
+        """Loads the QuantileNormalizer object from a string."""
+        normalizer = QuantileNormalizer()
+        normalizer.scaler = pickle.loads(scaler)
+        return normalizer
--- src/lm_polygraph/utils/normalize.py
+++ src/lm_polygraph/utils/normalize.py
@@ -2,9 +2,6 @@ from typing import List, Tuple, Dict
 import math
 
 import numpy as np
-from cir_model import CenteredIsotonicRegression
-from scipy.stats import binned_statistic
-from sklearn.preprocessing import QuantileTransformer, MinMaxScaler
 
 from lm_polygraph.utils.manager import UEManager
 from lm_polygraph.utils.common import seq_man_key
@@ -61,7 +58,7 @@ def get_mans_ues_metrics(man_paths: List[str], ue_method_names: List[str], gen_m
 
     ues = _concat_mans_data(mans_ues, ue_method_names)
     gen_metrics = _concat_mans_data(mans_gen_metrics, gen_metric_names)
-
+    
     return ues, gen_metrics
 
 
@@ -80,81 +77,7 @@ def filter_nans(gen_metrics: np.ndarray, ues: np.ndarray) -> Tuple[np.ndarray, n
     """
 
     nan_mask = np.isnan(gen_metrics) | np.isnan(ues)
-    ues = ues[~nan_mask]
     gen_metrics = gen_metrics[~nan_mask]
+    ues = ues[~nan_mask]
 
-    return ues, gen_metrics
-
-
-def fit_isotonic_pcc(gen_metrics: np.ndarray, ues: np.ndarray) -> CenteredIsotonicRegression:
-    """Fits centered isotonic regression to the gen_metrics and ues data."""
-    cir = CenteredIsotonicRegression(out_of_bounds='clip',
-                                     increasing=False,
-                                     y_min=0, y_max=1)
-    cir.fit(ues, gen_metrics)
-
-    return cir
-
-
-def _get_bin_indices(ue, num_bins):
-    per_bin = len(ue) / num_bins
-
-    lesser_bin = math.floor(per_bin)
-    greater_bin = math.ceil(per_bin)
-
-    n_lesser_bins = (greater_bin * num_bins) - len(ue)
-    n_greater_bins = num_bins - n_lesser_bins
-    
-    greater_start_index = n_lesser_bins * lesser_bin
-
-    bin_indices = [i * lesser_bin for i in range(n_lesser_bins)] + \
-                  [greater_start_index + i * greater_bin for i in range(n_greater_bins)] + \
-                  [len(ue) - 1]
-
-    return bin_indices
-
-
-def _get_bin_edges(ue, num_bins):
-    sorted_ue = np.sort(ue)
-    bin_indices = _get_bin_indices(sorted_ue, num_bins)
-    bin_edges = np.unique([sorted_ue[i] for i in bin_indices]).tolist()
-
-    return bin_edges
-
-
-def _get_bins(ue, metric, edges):
-    metric_bins = binned_statistic(ue,
-                                   metric,
-                                   bins=edges,
-                                   statistic='mean')
-
-    return metric_bins
-
-
-def fit_binned_pcc(gen_metrics: np.ndarray,
-                   ues: np.ndarray, num_bins: int) -> CenteredIsotonicRegression:
-    bin_edges = _get_bin_edges(ues, num_bins)
-    metric_bins = _get_bins(ues, gen_metrics, bin_edges)
-
-    binned_metric = metric_bins.statistic
-
-    return {
-        "binned_metric": binned_metric,
-        "bin_edges": bin_edges
-    }
-
-
-def fit_quantile(ues: np.ndarray) -> QuantileTransformer:
-    """Fits QuantileTransformer to the gen_metrics and ues data."""
-    scaler = QuantileTransformer(output_distribution='uniform')
-    scaler.fit(ues[:, None])
-
-    return scaler
-
-
-def fit_min_max(ues: np.ndarray) -> MinMaxScaler:
-    """Fits MinMaxScaler to the gen_metrics and ues data."""
-    scaler = MinMaxScaler(clip=True)
-    scaler.fit(ues[:, None])
-
-    return scaler
+    return gen_metrics, ues
