8f1dcbcee47ce61c4ba7f461594da59ed2593478
Roman Vashurin <rvashurin@gmail.com>
2024-06-14 17:15:54 +0400

    Clean up old stuff

--- src/lm_polygraph/utils/__init__.py
+++ src/lm_polygraph/utils/__init__.py
@@ -1,14 +1,3 @@
-import inspect
-
 from .model import WhiteboxModel, BlackboxModel
 from .manager import UEManager, estimate_uncertainty
 from .dataset import Dataset
-
-
-def polygraph_module_init(func):
-    def wrapper(*args, **kwargs):
-        breakpoint()
-        is_method = inspect.ismethod(func)
-        return func(*args, **kwargs)
-
-    return wrapper
