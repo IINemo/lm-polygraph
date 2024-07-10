2e1166562eb8d848144f3b33f504f080c6a3d3dd
Roman Vashurin <rvashurin@gmail.com>
2024-07-10 21:38:27 +0700

    Lint pass

--- docs/conf.py
+++ docs/conf.py
@@ -8,7 +8,6 @@
 
 import subprocess
 import sys
-import os
 from pathlib import Path
 
 # Assuming conf.py is located in /Users/sergeypetrakov/Documents/Documents/Skoltech_PhD/rtd_to_main/lm-polygraph/src/lm_polygraph/docs
@@ -24,22 +23,28 @@ sys.path.insert(0, str(root_path))
 subprocess.run(["pip", "install", "-e", "."], cwd=root_path)
 
 
-project = 'LM-Polygraph'
-copyright = '2023, MBZUAI'
-author = 'MBZUAI'
-release = '1.0.0'
+project = "LM-Polygraph"
+copyright = "2023, MBZUAI"
+author = "MBZUAI"
+release = "0.3.0"
 
 # -- General configuration ---------------------------------------------------
 # https://www.sphinx-doc.org/en/master/usage/configuration.html#general-configuration
 
-extensions = ["sphinx.ext.todo", "sphinx.ext.viewcode", "sphinx.ext.autodoc", 'sphinx.ext.duration',
-              'sphinx.ext.doctest', 'sphinx.ext.autosummary',]
+extensions = [
+    "sphinx.ext.todo",
+    "sphinx.ext.viewcode",
+    "sphinx.ext.autodoc",
+    "sphinx.ext.duration",
+    "sphinx.ext.doctest",
+    "sphinx.ext.autosummary",
+]
 
-templates_path = ['_templates']
-exclude_patterns = ['Thumbs.db', '.DS_Store'] #'_build', 
-#autodoc_mock_imports = ['lm_polygraph']
+templates_path = ["_templates"]
+exclude_patterns = ["Thumbs.db", ".DS_Store"]  # '_build',
+# autodoc_mock_imports = ['lm_polygraph']
 
-# , 'datasets>=2.14.2', "rouge-score>=0.0.4", "nlpaug>=1.1.10", 
+# , 'datasets>=2.14.2', "rouge-score>=0.0.4", "nlpaug>=1.1.10",
 #                         "scikit-learn", "tqdm>=4.64.1", "matplotlib>=3.6", "pandas>=1.3.5", "torch>=1.13.0",
 #                         "bs4", "transformers>=4.30.2", "nltk>=3.6.5", "sacrebleu>=1.5.0", "sentencepiece>=0.1.97",
 #                         "hf-lfs==0.0.3", "pytest>=4.4.1", "pytreebank>=0.2.7", "setuptools>=60.2.0",
@@ -47,9 +52,8 @@ exclude_patterns = ['Thumbs.db', '.DS_Store'] #'_build',
 #                         "einops", "accelerate", "bitsandbytes", "openai", "wget"]
 
 
-
 # -- Options for HTML output -------------------------------------------------
 # https://www.sphinx-doc.org/en/master/usage/configuration.html#options-for-html-output
 
-#html_theme = 'sphinx_rtd_theme'
-html_static_path = ['_static']
+# html_theme = 'sphinx_rtd_theme'
+html_static_path = ["_static"]
