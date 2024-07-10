0522c1167f1fc5caf0d2fe8c43cb164f1b4e824e
Roman Vashurin <rvashurin@gmail.com>
2024-07-10 10:51:02 +0700

    Add seq2seq example config

---
+++ examples/configs/model/default_seq2seq.py
@@ -0,0 +1,30 @@
+from transformers import AutoModelForSeq2SeqLM, AutoTokenizer, AutoConfig
+
+
+def load_model(model_path: str, device_map: str):
+    config = get_config(model_path)
+
+    model = AutoModelForSeq2SeqLM.from_pretrained(
+        model_path, trust_remote_code=True, device_map=device_map
+    )
+    model.eval()
+
+    return model
+
+
+def load_tokenizer(model_path: str):
+    config = get_config(model_path)
+
+    tokenizer = AutoTokenizer.from_pretrained(
+        model_path,
+        padding_side="left",
+        add_bos_token=True,
+    )
+    if tokenizer.pad_token is None:
+        tokenizer.pad_token = tokenizer.eos_token
+
+    return tokenizer
+
+
+def get_config(model_path: str):
+    return AutoConfig.from_pretrained(model_path, trust_remote_code=True)
