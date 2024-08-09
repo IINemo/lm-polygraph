8e023f366458c1ce201bf8c9ba404acf12492e79
Roman Vashurin <rvashurin@gmail.com>
2024-08-09 15:16:49 +0400

    Make ling1s work with common prompt

---
+++ examples/configs/instruct/model/default_seq2seq.py
@@ -0,0 +1,22 @@
+from transformers import AutoModelForSeq2SeqLM, AutoTokenizer
+
+
+def load_model(model_path: str, device_map: str):
+    model = AutoModelForSeq2SeqLM.from_pretrained(
+        model_path, trust_remote_code=True, device_map=device_map
+    )
+    model.eval()
+
+    return model
+
+
+def load_tokenizer(model_path: str):
+    tokenizer = AutoTokenizer.from_pretrained(
+        model_path,
+        padding_side="left",
+        add_bos_token=True,
+    )
+    if tokenizer.pad_token is None:
+        tokenizer.pad_token = tokenizer.eos_token
+
+    return tokenizer
