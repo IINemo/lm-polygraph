9f0ecf45236ef241ebcb6cee2620eaa9fa2350d2
SpeedOfMagic <vasilev.daniil18@gmail.com>
2024-08-12 17:39:39 +0000

    fix

--- dataset_builders/polygraph/polygraph.py
+++ dataset_builders/polygraph/polygraph.py
@@ -7,35 +7,24 @@ VERSION = datasets.Version("0.0.1")
 DATASET_CONFIG = {
     "xsum": {
         "name": "xsum",
-        "splits": [
-            "train",
-            "validation",
-            "test"
-        ],
+        "splits": ["train", "validation", "test"],
         "input_column": "document",
         "output_column": "summary",
-        "prompt": "Here's the text and it's short one-sentence summary.\n\nText:\n{text}\n\nSummary (one sentence):\n"
+        "prompt": "Here's the text and it's short one-sentence summary.\n\nText:\n{text}\n\nSummary (one sentence):\n",
     },
     "aeslc": {
         "name": "aeslc",
-        "splits": [
-            "train",
-            "validation",
-            "test"
-        ],
+        "splits": ["train", "validation", "test"],
         "input_column": "email_body",
         "output_column": "subject_line",
-        "prompt": "Write a short subject line for the email. Output only the subject line itself.\n\nEmail:\n{text}\n\nSubject line:\n"
+        "prompt": "Write a short subject line for the email. Output only the subject line itself.\n\nEmail:\n{text}\n\nSubject line:\n",
     },
     "trivia_qa_tiny": {
         "name": "SpeedOfMagic/trivia_qa_tiny",
-        "splits": [
-            "train",
-            "test"
-        ],
+        "splits": ["train", "test"],
         "input_column": "question",
-        "output_column": "answer"
-    }
+        "output_column": "answer",
+    },
 }
 
 
@@ -51,9 +40,21 @@ class Polygraph(datasets.GeneratorBasedBuilder):
 
     BUILDER_CONFIG_CLASS = PolygraphConfig
     BUILDER_CONFIGS = [
-        datasets.BuilderConfig(name="xsum", version=VERSION, description="Dataset xsum, processed by lm-polygraph"),
-        datasets.BuilderConfig(name="aeslc", version=VERSION, description="Dataset aeslc, processed by lm-polygraph"),
-        datasets.BuilderConfig(name="trivia_qa_tiny", version=VERSION, description="Dataset SpeedOfMagic/trivia_qa_tiny, processed by lm-polygraph"),
+        datasets.BuilderConfig(
+            name="xsum",
+            version=VERSION,
+            description="Dataset xsum, processed by lm-polygraph",
+        ),
+        datasets.BuilderConfig(
+            name="aeslc",
+            version=VERSION,
+            description="Dataset aeslc, processed by lm-polygraph",
+        ),
+        datasets.BuilderConfig(
+            name="trivia_qa_tiny",
+            version=VERSION,
+            description="Dataset SpeedOfMagic/trivia_qa_tiny, processed by lm-polygraph",
+        ),
     ]
 
     def _info(self):
@@ -76,18 +77,22 @@ class Polygraph(datasets.GeneratorBasedBuilder):
 
     def _split_generators(self, dl_manager):
         config = DATASET_CONFIG[self.config.name]
-        dataset = datasets.load_dataset(
-            config["name"], trust_remote_code=True
-        )
+        dataset = datasets.load_dataset(config["name"], trust_remote_code=True)
 
         def download_custom_dataset(src_url: str, dst_path: str):
             split = src_url.split("_")[-1]
-            x, y = self._prepare_dataset(dataset[split], config["input_column"], config["output_column"], config.get("prompt"))
+            x, y = self._prepare_dataset(
+                dataset[split],
+                config["input_column"],
+                config["output_column"],
+                config.get("prompt"),
+            )
             result_dataset = datasets.Dataset.from_dict({"input": x, "output": y})
             result_dataset.save_to_disk(dst_path)
 
         downloaded_files = dl_manager.download_custom(
-            {split: f"{config['name']}_{split}" for split in config["splits"]}, download_custom_dataset
+            {split: f"{config['name']}_{split}" for split in config["splits"]},
+            download_custom_dataset,
         )
 
         result = []
