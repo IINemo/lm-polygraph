5f83babd1f596068e96450a2f5548dd20a333a54
Roman Vashurin <rvashurin@gmail.com>
2024-08-27 15:28:50 +0400

    Add configs for baseline experiments

--- src/lm_polygraph/estimators/p_true_empirical.py
+++ src/lm_polygraph/estimators/p_true_empirical.py
@@ -6,8 +6,8 @@ from .estimator import Estimator
 
 
 class PTrueEmpirical(Estimator):
-    def __init__(self):
-        self.num_samples = 10
+    def __init__(self, num_samples: int = 10):
+        self.num_samples = num_samples
         super().__init__(["input_texts", "greedy_texts"], "sequence")
 
     def __str__(self):
@@ -17,7 +17,7 @@ class PTrueEmpirical(Estimator):
         prompts = stats["input_texts"]
         guesses = stats["greedy_texts"]
         model = stats["model"]
-        
+
         ues = []
         for prompt, guess in zip(prompts, guesses):
             input = f"Question:\n\n{prompt}\n\nProposed Answer: {guess}\n\nIs the proposed answer:\n\tTrue or\n\tFalse?\nThe proposed answer is: "
