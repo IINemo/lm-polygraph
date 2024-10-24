145ce4579e70dd3d3e6f91ede2869c637c6c3018
Daniil Vasilev <steam.haver@gmail.com>
2024-10-24 23:22:05 +0300

    Delete dataset_builders/script.py

--- dataset_builders/script.py
+++
@@ -1,9 +0,0 @@
-import subprocess
-
-
-def bash(cmd):
-    return subprocess.run(cmd, shell=True)
-
-for dataset in "wmt14_fren,wmt19_deen".split(","):
-    print("Processing dataset", dataset)
-    bash(f"python3 manager.py --dataset {dataset} -p")
