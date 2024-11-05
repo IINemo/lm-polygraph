1156505d73f2cb62ea3c2a9b0c2bd50209d181f7
Roman Vashurin <rvashurin@gmail.com>
2024-11-05 18:33:10 +0400

    Another lint

--- src/lm_polygraph/utils/dataset.py
+++ src/lm_polygraph/utils/dataset.py
@@ -198,7 +198,7 @@ class Dataset:
             if y_column is not None:
                 y = dataset[y_column]
             else:
-                y = ['' for _ in range(len(x))]
+                y = ["" for _ in range(len(x))]
 
         return Dataset(x, y, batch_size)
 
