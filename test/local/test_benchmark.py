0ea1c0b4fcd63d4fdf4b4db594eb3bf2e6fb1233
Roman Vashurin <rvashurin@gmail.com>
2024-11-05 15:38:39 +0400

    Change test

--- test/local/test_benchmark.py
+++ test/local/test_benchmark.py
@@ -3,6 +3,7 @@ import pathlib
 import os
 import torch
 import sys
+import json
 
 from lm_polygraph.utils.manager import UEManager
 
@@ -17,18 +18,15 @@ def get_device():
     else:
         return "cpu"
 
-def load_input_texts(dataset, method=None):
-    filename = f"{dataset}"
-    if method:
-        filename += f"_{method}"
-
-    with open(f"{pwd()}/fixtures/{filename}.txt", "r") as f:
-        return f.read()
+@pytest.fixture(scope="module")
+def reference_texts(reference):
+    with open(f"{pwd()}/fixtures/input_output_fixtures.json") as f:
+        return json.load(f)
 
 def pwd():
     return pathlib.Path(__file__).parent.resolve()
 
-def check_result(dataset, exec_result, method=None):
+def check_result(dataset, exec_result, reference, method=None):
     assert (
         exec_result.returncode == 0
     ), f"polygraph_eval returned code {exec_result.returncode} != 0"
@@ -38,13 +36,17 @@ def check_result(dataset, exec_result, method=None):
     if method is None:
         assert len(man.estimations[("sequence", "MaximumSequenceProbability")]) == 2
     try:
-        assert man.stats["input_texts"][0] == load_input_texts(dataset, method)
+        key = dataset
+        if method:
+            key += f"_{method}"
+
+        assert man.stats["input_texts"][0] == reference[key + "_input"] 
+        assert man.stats["target_texts"][0] == reference[key + "_output"]
     except:
         sys.stdout.flush()
         breakpoint()
 
     os.remove(f"{pwd()}/ue_manager_seed1")
-
 
 # ================= TEST CASES ==================
 
@@ -62,39 +64,39 @@ def run_eval(dataset):
     return subprocess.run(command, shell=True)
 
 
-def test_coqa():
+def test_coqa(reference):
     exec_result = run_eval("coqa")
-    check_result('coqa', exec_result)
+    check_result('coqa', exec_result, reference):
 
 
-def test_triviaqa():
+def test_triviaqa(reference):
     exec_result = run_eval("triviaqa")
-    check_result('triviaqa', exec_result)
+    check_result('triviaqa', exec_result, reference):
 
 
-def test_mmlu():
+def test_mmlu(reference):
     exec_result = run_eval("mmlu")
-    check_result('mmlu', exec_result)
+    check_result('mmlu', exec_result, reference):
 
 
-def test_gsm8k():
+def test_gsm8k(reference):
     exec_result = run_eval("gsm8k")
-    check_result('gsm8k', exec_result)
+    check_result('gsm8k', exec_result, reference):
 
 
-def test_wmt14_fren():
+def test_wmt14_fren(reference):
     exec_result = run_eval("wmt14_fren")
-    check_result('wmt14_fren', exec_result)
+    check_result('wmt14_fren', exec_result, reference):
 
 
-def test_wmt19_deen():
+def test_wmt19_deen(reference):
     exec_result = run_eval("wmt19_deen")
-    check_result('wmt19_deen', exec_result)
+    check_result('wmt19_deen', exec_result, reference):
 
 
-def test_xsum():
+def test_xsum(reference):
     exec_result = run_eval("xsum")
-    check_result('xsum', exec_result)
+    check_result('xsum', exec_result, reference):
 
 
 # ================= INSTRUCT TEST CASES ==================
@@ -114,20 +116,20 @@ def run_instruct_eval(dataset, method):
 
 METHODS = ["ling_1s", "verb_1s_top1", "verb_1s_topk", "verb_2s_top1", "verb_2s_topk", "verb_2s_cot", "empirical_baselines"]
 
-def test_coqa_instruct():
+def test_coqa_instruct(reference):
     for method in METHODS:
         exec_result = run_instruct_eval("coqa", method)
-        check_result('coqa', exec_result, method)
+        check_result('coqa', exec_result, reference, method)
 
-def test_triviaqa_instruct():
+def test_triviaqa_instruct(reference):
     for method in METHODS:
         exec_result = run_instruct_eval("triviaqa", method)
-        check_result('triviaqa', exec_result, method)
+        check_result('triviaqa', exec_result, reference, method)
 
-def test_mmlu_instruct():
+def test_mmlu_instruct(reference):
     for method in METHODS:
         exec_result = run_instruct_eval("mmlu", method)
-        check_result('mmlu', exec_result, method)
+        check_result('mmlu', exec_result, reference, method)
 
 
 # ================= CLAIM-LEVEL ==================
@@ -146,14 +148,15 @@ def check_claim_level_result(dataset):
     man = UEManager.load(f"{pwd()}/ue_manager_seed1")
 
     try:
-        assert man.stats["input_texts"][0] == load_input_texts(dataset)
+        assert man.stats["input_texts"][0] == reference[dataset + "_input"] 
+        assert man.stats["target_texts"][0] == reference[dataset + "_output"]
     except:
         sys.stdout.flush()
         breakpoint()
 
     os.remove(f"{pwd()}/ue_manager_seed1")
 
-def test_person_bio():
+def test_person_bio(reference):
     base_dataset_name = "person_bio"
     langs = ["en", "ru", "zh", "ar"]
 
