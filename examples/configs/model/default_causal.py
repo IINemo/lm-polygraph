457f94bcd39c3e532750a99e3e79c9fb1c417fb4
Roman Vashurin <rvashurin@gmail.com>
2024-12-05 13:06:39 +0400

    Add dtype to load args

--- examples/configs/model/default_causal.py
+++ examples/configs/model/default_causal.py
@@ -1,9 +1,10 @@
 from transformers import AutoModelForCausalLM, AutoTokenizer
+import torch
 
-
-def load_model(model_path: str, device_map: str):
+def load_model(model_path: str, device_map: str, dtype: str = "float32"):
+    dtype = getattr(torch, dtype)
     model = AutoModelForCausalLM.from_pretrained(
-        model_path, trust_remote_code=True, device_map=device_map
+        model_path, trust_remote_code=True, device_map=device_map, torch_dtype=dtype
     )
     model.eval()
 
