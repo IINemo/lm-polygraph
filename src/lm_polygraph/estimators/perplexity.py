b9646f0e1fe49ae9496b97c37b0dc88a6b83ce0f
silvimica <mayagoloburda@gmail.com>
2024-12-11 17:24:42 +0400

    add MaxSampledPerplexity

--- src/lm_polygraph/estimators/perplexity.py
+++ src/lm_polygraph/estimators/perplexity.py
@@ -27,3 +27,23 @@ class SampledPerplexity(Estimator):
         log_likelihoods = stats["sample_log_likelihoods"]
         ppl = [np.mean(sample_log_likelihoods[0]) for sample_log_likelihoods in log_likelihoods]
         return -np.array(ppl)
+
+class MaxSampledPerplexity(Estimator):
+    def init(self):
+        super().init(["sample_log_likelihoods"], "sequence")
+
+    def str(self):
+        return "MaxSampledPerplexity"
+
+    def call(self, stats: Dict[str, np.ndarray]) -> np.ndarray:
+        log_likelihoods = stats["sample_log_likelihoods"]
+        
+        ppl_per_sample = [
+            [-np.mean(sequence) for sequence in sample_log_likelihoods]
+            for sample_log_likelihoods in log_likelihoods
+        ]
+
+        # Find the maximum perplexity for each set of samples
+        max_ppl = [max(ppl_sample) for ppl_sample in ppl_per_sample]
+
+        return -np.array(max_ppl)
