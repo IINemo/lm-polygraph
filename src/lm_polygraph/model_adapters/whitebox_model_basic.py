ab1b2de6907ac2f7d8a50daafb8d266ddf3a5257
iinemo <artemshelmanov@gmail.com>
2025-01-30 10:40:34 +0400

    Fix problems with tokenization

--- src/lm_polygraph/model_adapters/whitebox_model_basic.py
+++ src/lm_polygraph/model_adapters/whitebox_model_basic.py
@@ -1,22 +1,37 @@
 from lm_polygraph.utils.model import Model
+from lm_polygraph.utils.generation_parameters import GenerationParameters
 
-from typing import List
+from typing import List, Dict
 
 from transformers import AutoModelForCausalLM, AutoTokenizer
+
 
 
 class WhiteboxModelBasic(Model):
     """Basic whitebox model adapter for using in stat calculators and uncertainty estimators."""
 
-    def __init__(self, model: AutoModelForCausalLM, tokenizer: AutoTokenizer):
+    def __init__(self, model: AutoModelForCausalLM, tokenizer: AutoTokenizer, 
+                 tokenizer_args: Dict, parameters: GenerationParameters = GenerationParameters(), model_type=""):
         self.model = model
         self.tokenizer = tokenizer
+        self.tokenizer_args = tokenizer_args
+        self.generation_parameters = parameters
+        self.model_type = model_type
 
     def generate(self, *args, **kwargs):
         return self.model.generate(*args, **kwargs)
 
     def tokenize(self, *args, **kwargs):
-        return self.tokenizer(*args, **kwargs)
+        return self.tokenizer(*args, **self.tokenizer_args, **kwargs)
+    
+    def device(self):
+        """
+        Returns the device the model is currently loaded on.
+
+        Returns:
+            str: device string.
+        """
+        return self.model.device
 
     def __call__(self, *args, **kwargs):
         return self.model(*args, **kwargs)
