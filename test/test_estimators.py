85c736631048877f7f0a96bf69867663551a902d
Artem Vazhentsev <artiomvazh99@gmail.com>
2025-03-10 18:12:43 +0300

    black

--- test/test_estimators.py
+++ test/test_estimators.py
@@ -222,6 +222,7 @@ def test_kernel_language_entropy(model):
     ue = estimate_uncertainty(model, estimator, INPUT)
     assert isinstance(ue.uncertainty, float)
 
+
 def test_luq(model):
     estimator = LUQ()
     ue = estimate_uncertainty(model, estimator, INPUT)
