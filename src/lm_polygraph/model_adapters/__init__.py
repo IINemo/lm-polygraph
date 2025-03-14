045d58db1fdc95c5f78e23ca911aa7406b86fbeb
Artem Vazhentsev <artiomvazh99@gmail.com>
2025-03-14 11:23:55 +0300

    upd vLLM

--- src/lm_polygraph/model_adapters/__init__.py
+++ src/lm_polygraph/model_adapters/__init__.py
@@ -1,3 +1,4 @@
 from .whitebox_model_basic import WhiteboxModelBasic
+from .whitebox_model_vllm import WhiteboxModelvLLM
 from lm_polygraph.utils.model import WhiteboxModel
 from lm_polygraph.utils.model import BlackboxModel
