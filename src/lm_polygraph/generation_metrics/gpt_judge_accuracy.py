aa577fcd9e6c11b787248efd87fb8a1f2cf24579
Roman Vashurin <rvashurin@gmail.com>
2025-03-29 19:46:41 +0400

    Add multiref support without aggregation, some other tweaks

--- src/lm_polygraph/generation_metrics/gpt_judge_accuracy.py
+++ src/lm_polygraph/generation_metrics/gpt_judge_accuracy.py
@@ -6,6 +6,7 @@ from typing import Dict, List
 import re 
 log = logging.getLogger("lm_polygraph")
 import os 
+from tqdm import tqdm
 
 class GptAccuracyMetric(GenerationMetric):
     """
@@ -15,13 +16,15 @@ class GptAccuracyMetric(GenerationMetric):
     def __init__(self, model="gpt-4o-mini", sample=False, sample_strategy="First", api_key=None):
         if sample:
             super().__init__([
+                "no_fewshot_input_texts",
                 "first_sample_texts",
                 "best_sample_texts",
                 "best_normalized_sample_texts",
                 "input_texts"],
                 "sequence")
         else:
-            super().__init__(["greedy_texts", "input_texts"], "sequence")
+            super().__init__(["no_fewshot_input_texts", "greedy_texts", "input_texts"], "sequence")
+
         self.sample = sample
         self.sample_strategy = sample_strategy
         self.model = model
@@ -32,28 +35,41 @@ class GptAccuracyMetric(GenerationMetric):
         if self.sample == True:
             return f"GptAccuracy_{self.model}_{self.sample_strategy}"
         return f"GptAccuracy_{self.model}"
-    
+
     def _filter_input(self, input):
         matches = re.findall(r"Question:\s*(.*?)\nAnswer:", input, re.DOTALL)
         if matches:
             return matches[-1].strip()
         return input
+
     def _gpt_compare(self, output: str, target: str, question: str) -> int:
-        prompt = (
-            f"You are a text evaluator. The model was asked the following question: {question.strip()}.\n"
-            "The 'Generated' text is a model's response. The 'Target' is the correct answer.\n"
-            "If the generated answer correctly answers the question based on the target, return 1.\n"
-            "If it is wrong, return 0.\n"
-            "Respond ONLY with a single digit: 1 or 0.\n\n"
-            f"Generated: {output.strip()}\n"
-            f"Target: {target.strip()}"
-        )
+        if type(target) == list:
+            str_target = ", ".join(target)
+            prompt = (
+                f"You are a text evaluator. The model was asked the following question:\n{question}\n"
+                "The 'Generated' text is a model's response. The 'Target' is the list of possible correct answers.\n"
+                "If the generated answer correctly answers the question (matches one of the target responses), return 1.\n"
+                "If it is wrong, return 0.\n"
+                "Respond ONLY with a single digit: 1 or 0.\n\n"
+                f"Generated: {output.strip()}\n"
+                f"Target list: {str_target.strip()}"
+            )
+        else:
+            prompt = (
+                f"You are a text evaluator. The model was asked the following question:\n{question}\n"
+                "The 'Generated' text is a model's response. The 'Target' is the correct answer.\n"
+                "If the generated answer correctly answers the question based on the target, return 1.\n"
+                "If it is wrong, return 0.\n"
+                "Respond ONLY with a single digit: 1 or 0.\n\n"
+                f"Generated: {output.strip()}\n"
+                f"Target: {target.strip()}"
+            )
 
         try:
             response = openai.ChatCompletion.create(
                 model=self.model,
                 messages=[
-                    {"role": "system", "content": "You are a strict evaluator of text similarity."},
+                    {"role": "system", "content": "You are a strict evaluator of correctness of the model's response."},
                     {"role": "user", "content": prompt}
                 ],
                 temperature=0,
@@ -62,6 +78,7 @@ class GptAccuracyMetric(GenerationMetric):
             )
 
             raw_reply = response['choices'][0]['message']['content'].strip()
+
             return int(raw_reply) if raw_reply in ['0', '1'] else 0
 
         except Exception as e:
@@ -82,8 +99,9 @@ class GptAccuracyMetric(GenerationMetric):
             gen_texts = stats["greedy_texts"]
 
         results = []
-        input_texts = [self._filter_input(text) for text in stats["input_texts"]]
-        for output, target, input in zip(gen_texts, target_texts, input_texts):
+        input_texts = stats["no_fewshot_input_texts"]
+
+        for output, target, input in tqdm(zip(gen_texts, target_texts, input_texts)):
             score = self._gpt_compare(output, target,input)
             results.append(score)
 
