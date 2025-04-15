45392a8c776eea9e10295fd7b8a4b12a6061fea2
alfekka <milarv99@gmail.com>
2025-04-15 17:07:49 +0300

    removed high comp test

--- test/test_estimators_visual.py
+++ test/test_estimators_visual.py
@@ -5,7 +5,7 @@ from transformers import AutoModelForVision2Seq, AutoProcessor
 
 from lm_polygraph import estimate_uncertainty
 from lm_polygraph.estimators import *
-from lm_polygraph.model_adapters.visual_whitebox_model import VisualWhiteboxModel
+from lm_polygraph.model_adapters.whitebox_visual import VisualWhiteboxModel
 
 INPUT = "<grounding>An image of?"
 
@@ -46,67 +46,133 @@ def test_perplexity(model):
     assert isinstance(ue.uncertainty, float)
 
 
-def test_mean_token_entropy(model):
-    estimator = MeanTokenEntropy()
-    ue = estimate_uncertainty(model, estimator, INPUT)
-    assert isinstance(ue.uncertainty, float)
+# def test_mean_token_entropy(model):
+#     estimator = MeanTokenEntropy()
+#     ue = estimate_uncertainty(model, estimator, INPUT)
+#     assert isinstance(ue.uncertainty, float)
 
 
-def test_mean_pointwise_mutual_information(model):
-    estimator = MeanPointwiseMutualInformation()
-    ue = estimate_uncertainty(model, estimator, INPUT)
-    assert isinstance(ue.uncertainty, float)
+# def test_mean_pointwise_mutual_information(model):
+#     estimator = MeanPointwiseMutualInformation()
+#     ue = estimate_uncertainty(model, estimator, INPUT)
+#     assert isinstance(ue.uncertainty, float)
 
 
-def test_mean_conditional_pointwise_mutual_information(model):
-    estimator = MeanConditionalPointwiseMutualInformation()
-    ue = estimate_uncertainty(model, estimator, INPUT)
-    assert isinstance(ue.uncertainty, float)
+# def test_mean_conditional_pointwise_mutual_information(model):
+#     estimator = MeanConditionalPointwiseMutualInformation()
+#     ue = estimate_uncertainty(model, estimator, INPUT)
+#     assert isinstance(ue.uncertainty, float)
 
 
-def test_claim_conditioned_probability(model):
-    estimator = ClaimConditionedProbability()
-    ue = estimate_uncertainty(model, estimator, INPUT)
-    assert isinstance(ue.uncertainty, float)
+# def test_claim_conditioned_probability(model):
+#     estimator = ClaimConditionedProbability()
+#     ue = estimate_uncertainty(model, estimator, INPUT)
+#     assert isinstance(ue.uncertainty, float)
 
 
-def test_degmat_nli_entail(model):
-    estimator = DegMat(similarity_score="NLI_score", affinity="entail")
-    ue = estimate_uncertainty(model, estimator, INPUT)
-    assert isinstance(ue.uncertainty, float)
+# def test_lexical_similarity_rouge1(model):
+#     estimator = LexicalSimilarity(metric="rouge1")
+#     ue = estimate_uncertainty(model, estimator, INPUT)
+#     assert isinstance(ue.uncertainty, float)
 
 
-def test_eccentricity_nli_entail(model):
-    estimator = Eccentricity(similarity_score="NLI_score", affinity="entail")
-    ue = estimate_uncertainty(model, estimator, INPUT)
-    assert isinstance(ue.uncertainty, float)
+# def test_lexical_similarity_rouge2(model):
+#     estimator = LexicalSimilarity(metric="rouge2")
+#     ue = estimate_uncertainty(model, estimator, INPUT)
+#     assert isinstance(ue.uncertainty, float)
 
 
-def test_semantic_entropy(model):
-    estimator = SemanticEntropy()
-    ue = estimate_uncertainty(model, estimator, INPUT)
-    assert isinstance(ue.uncertainty, float)
+# def test_lexical_similarity_rougel(model):
+#     estimator = LexicalSimilarity(metric="rougeL")
+#     ue = estimate_uncertainty(model, estimator, INPUT)
+#     assert isinstance(ue.uncertainty, float)
 
 
-def test_renyi_neg(model):
-    estimator = RenyiNeg()
-    ue = estimate_uncertainty(model, estimator, INPUT)
-    assert isinstance(ue.uncertainty, float)
+# def test_lexical_similarity_bleu(model):
+#     estimator = LexicalSimilarity(metric="BLEU")
+#     ue = estimate_uncertainty(model, estimator, INPUT)
+#     assert isinstance(ue.uncertainty, float)
 
 
-def test_fisher_rao(model):
-    estimator = FisherRao()
-    ue = estimate_uncertainty(model, estimator, INPUT)
-    assert isinstance(ue.uncertainty, float)
+# def test_eigval_laplacian_nli_entail(model):
+#     estimator = EigValLaplacian(similarity_score="NLI_score", affinity="entail")
+#     ue = estimate_uncertainty(model, estimator, INPUT)
+#     assert isinstance(ue.uncertainty, float)
 
 
-def test_kernel_language_entropy(model):
-    estimator = KernelLanguageEntropy()
-    ue = estimate_uncertainty(model, estimator, INPUT)
-    assert isinstance(ue.uncertainty, float)
+# def test_eigval_laplacian_nli_contra(model):
+#     estimator = EigValLaplacian(similarity_score="NLI_score", affinity="contra")
+#     ue = estimate_uncertainty(model, estimator, INPUT)
+#     assert isinstance(ue.uncertainty, float)
 
 
-def test_eigenscore(model):
-    estimator = EigenScore()
-    ue = estimate_uncertainty(model, estimator, INPUT)
-    assert isinstance(ue.uncertainty, float)
+# def test_eigval_laplacian_jaccard(model):
+#     estimator = EigValLaplacian(similarity_score="Jaccard_score")
+#     ue = estimate_uncertainty(model, estimator, INPUT)
+#     assert isinstance(ue.uncertainty, float)
+
+
+# def test_degmat_nli_entail(model):
+#     estimator = DegMat(similarity_score="NLI_score", affinity="entail")
+#     ue = estimate_uncertainty(model, estimator, INPUT)
+#     assert isinstance(ue.uncertainty, float)
+
+
+# def test_eccentricity_nli_entail(model):
+#     estimator = Eccentricity(similarity_score="NLI_score", affinity="entail")
+#     ue = estimate_uncertainty(model, estimator, INPUT)
+#     assert isinstance(ue.uncertainty, float)
+
+
+# def test_semantic_entropy(model):
+#     estimator = SemanticEntropy()
+#     ue = estimate_uncertainty(model, estimator, INPUT)
+#     assert isinstance(ue.uncertainty, float)
+
+
+# def test_sar(model):
+#     estimator = SAR()
+#     ue = estimate_uncertainty(model, estimator, INPUT)
+#     assert isinstance(ue.uncertainty, float)
+
+
+# def test_token_sar(model):
+#     estimator = TokenSAR()
+#     ue = estimate_uncertainty(model, estimator, INPUT)
+#     assert isinstance(ue.uncertainty, float)
+
+
+# def test_sentence_sar(model):
+#     estimator = SentenceSAR()
+#     ue = estimate_uncertainty(model, estimator, INPUT)
+#     assert isinstance(ue.uncertainty, float)
+
+
+# def test_renyi_neg(model):
+#     estimator = RenyiNeg()
+#     ue = estimate_uncertainty(model, estimator, INPUT)
+#     assert isinstance(ue.uncertainty, float)
+
+
+# def test_fisher_rao(model):
+#     estimator = FisherRao()
+#     ue = estimate_uncertainty(model, estimator, INPUT)
+#     assert isinstance(ue.uncertainty, float)
+
+
+# def test_kernel_language_entropy(model):
+#     estimator = KernelLanguageEntropy()
+#     ue = estimate_uncertainty(model, estimator, INPUT)
+#     assert isinstance(ue.uncertainty, float)
+
+
+# def test_luq(model):
+#     estimator = LUQ()
+#     ue = estimate_uncertainty(model, estimator, INPUT)
+#     assert isinstance(ue.uncertainty, float)
+
+
+# def test_eigenscore(model):
+#     estimator = EigenScore()
+#     ue = estimate_uncertainty(model, estimator, INPUT)
+#     assert isinstance(ue.uncertainty, float)
