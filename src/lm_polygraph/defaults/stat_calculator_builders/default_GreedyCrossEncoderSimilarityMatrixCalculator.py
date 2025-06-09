616a213d309797ca62d25f628b3b4e5d2c19ec7e
Roman Vashurin <rvashurin@gmail.com>
2025-06-10 00:13:00 +0300

    Some fixes to cocoa

--- src/lm_polygraph/defaults/stat_calculator_builders/default_GreedyCrossEncoderSimilarityMatrixCalculator.py
+++ src/lm_polygraph/defaults/stat_calculator_builders/default_GreedyCrossEncoderSimilarityMatrixCalculator.py
@@ -1,9 +1,9 @@
-from lm_polygraph.stat_calculators.cross_encoder_similarity import (
-    CrossEncoderSimilarityMatrixCalculator,
+from lm_polygraph.stat_calculators.greedy_cross_encoder_similarity import (
+    GreedyCrossEncoderSimilarityMatrixCalculator,
 )
 
 
 def load_stat_calculator(config, builder):
-    return CrossEncoderSimilarityMatrixCalculator(
+    return GreedyCrossEncoderSimilarityMatrixCalculator(
         config.batch_size, config.cross_encoder_name
     )
--- src/lm_polygraph/utils/manager.py
+++ src/lm_polygraph/utils/manager.py
@@ -86,8 +86,7 @@ def order_calculators(
             stats = stats[1:]
             ordered.append(stat)
             for new_stat in stat_calculators[stat].meta_info()[0]:
-                have_stats.add(new_stat)
-
+               have_stats.add(new_stat)
     return ordered, have_stats
 
 
@@ -226,7 +225,6 @@ class UEManager:
                 )  # remove blackbox_X from stats only if X is already in stats to remove duplicated run of stat calculator
             )
         ]  # below in calculate() we copy X in blackbox_X
-
         self.stat_calculators = self.factory_stat_calc(
             [self.stat_calculators_dict[c] for c in stats]
         )
