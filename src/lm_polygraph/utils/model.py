7896acd247ce00be56755be55fa89f8da5ed2aa7
Artem Vazhentsev <artiomvazh99@gmail.com>
2025-06-16 10:46:43 +0300

    added parameter instruct

--- src/lm_polygraph/utils/model.py
+++ src/lm_polygraph/utils/model.py
@@ -398,6 +398,7 @@ class WhiteboxModel(Model):
         model_path: str = None,
         model_type: str = "CausalLM",
         generation_parameters: GenerationParameters = GenerationParameters(),
+        instruct: bool = False,
     ):
         """
         Parameters:
@@ -411,6 +412,7 @@ class WhiteboxModel(Model):
         self.model = model
         self.tokenizer = tokenizer
         self.generation_parameters = generation_parameters
+        self.instruct = instruct
 
     def _validate_args(self, args):
         """
@@ -678,7 +680,7 @@ class WhiteboxModel(Model):
         """
         # Apply chat template if tokenizer has it
         add_start_symbol = True
-        if self.tokenizer.chat_template is not None:
+        if self.instruct:
             formatted_texts = []
             for chat in texts:
                 if isinstance(chat, str):
@@ -690,7 +692,6 @@ class WhiteboxModel(Model):
             texts = formatted_texts
 
             add_start_symbol = False
-
         return self.tokenizer(
             texts,
             padding=True,
