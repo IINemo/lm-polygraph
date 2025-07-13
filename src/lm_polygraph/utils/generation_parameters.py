dc130d68f8ad642bcfb3eaa7015f32bf3b971920
Dr. Artem Shelmanov <ArtemShelmanov@gmail.com>
2025-07-13 23:31:49 +0400

    Fix max new tokens in estimate_uncertainty function (#373)
    * Fix parameter max_new_tokens in the estimate_uncertainty function
    
    * Fix
    
    * Fix linter issues

--- src/lm_polygraph/utils/generation_parameters.py
+++ src/lm_polygraph/utils/generation_parameters.py
@@ -34,3 +34,4 @@ class GenerationParameters:
     repetition_penalty: float = 1.0
     generate_until: tuple = ()
     allow_newlines: bool = True
+    max_new_tokens: int = 100  # Works only with estimate_uncertainty utility function
