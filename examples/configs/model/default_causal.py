703053e6a3ea19357506289b32c0e34ec28b0b8f
alfekka <milarv99@gmail.com>
2025-07-16 13:57:39 +0300

    fix

--- examples/configs/model/default_causal.py
+++ examples/configs/model/default_causal.py
@@ -3,7 +3,10 @@ from transformers import AutoModelForCausalLM, AutoTokenizer
 
 def load_model(model_path: str, device_map: str):
     model = AutoModelForCausalLM.from_pretrained(
-        model_path, trust_remote_code=True, device_map=device_map, attn_implementation="eager",
+        model_path,
+        trust_remote_code=True,
+        device_map=device_map,
+        attn_implementation="eager",
     )
     model.eval()
 
