bf1e8a03dc20e8f797d0fc2de566bcb16bb99f72
Roman Vashurin <rvashurin@gmail.com>
2025-08-12 12:22:59 +0400

    Publish all

--- dataset_builders/publish_all_datasets.sh
+++ dataset_builders/publish_all_datasets.sh
@@ -13,12 +13,12 @@ fi
 
 DATASETS=(
   # base
-  #trivia_qa_tiny
-  #aeslc
+  trivia_qa_tiny
+  aeslc
 
-  ## babi_qa
-  #babi_qa
-
+  # babi_qa
+  babi_qa
+    
   # coqa
   coqa
   coqa_empirical_baselines
@@ -29,62 +29,62 @@ DATASETS=(
   coqa_verb_2s_top1
   coqa_verb_2s_topk
   coqa_simple_instruct
-
+    
   # gsm8k
-  #gsm8k
-  #gsm8k_simple_instruct
+  gsm8k
+  gsm8k_simple_instruct
 
-  ## mmlu
-  #mmlu
-  #mmlu_empirical_baselines
-  #mmlu_ling_1s
-  #mmlu_verb_1s_top1
-  #mmlu_verb_1s_topk
-  #mmlu_verb_2s_cot
-  #mmlu_verb_2s_top1
-  #mmlu_verb_2s_topk
-  #mmlu_simple_instruct
+  # mmlu
+  mmlu
+  mmlu_empirical_baselines
+  mmlu_ling_1s
+  mmlu_verb_1s_top1
+  mmlu_verb_1s_topk
+  mmlu_verb_2s_cot
+  mmlu_verb_2s_top1
+  mmlu_verb_2s_topk
+  mmlu_simple_instruct
 
-  ## person
-  #person_bio_ar
-  #person_bio_en
-  #person_bio_ru
-  #person_bio_zh
+  # person
+  person_bio_ar
+  person_bio_en
+  person_bio_ru
+  person_bio_zh
 
-  ## triviaqa
-  #triviaqa
-  #triviaqa_empirical_baselines
-  #triviaqa_ling_1s
-  #triviaqa_verb_1s_top1
-  #triviaqa_verb_1s_topk
-  #triviaqa_verb_2s_cot
-  #triviaqa_verb_2s_top1
-  #triviaqa_verb_2s_topk
-  #triviaqa_simple_instruct
+  # triviaqa
+  triviaqa
+  triviaqa_empirical_baselines
+  triviaqa_ling_1s
+  triviaqa_verb_1s_top1
+  triviaqa_verb_1s_topk
+  triviaqa_verb_2s_cot
+  triviaqa_verb_2s_top1
+  triviaqa_verb_2s_topk
+  triviaqa_simple_instruct
 
-  ## wiki
-  #wiki_bio
+  # wiki
+  wiki_bio
 
-  ## wmt
-  #wmt14_deen
-  #wmt14_fren
-  #wmt14_fren_simple_instruct
-  #wmt19_deen
-  #wmt19_deen_simple_instruct
-  #wmt19_ruen
-  #wmt19_ruen_simple_instruct
+  # wmt
+  wmt14_deen
+  wmt14_fren
+  wmt14_fren_simple_instruct
+  wmt19_deen
+  wmt19_deen_simple_instruct
+  wmt19_ruen
+  wmt19_ruen_simple_instruct
 
-  ## truthfulqa
-  #truthfulqa
-  #truthfulqa_simple_instruct
+  # truthfulqa
+  truthfulqa
+  truthfulqa_simple_instruct
 
-  ## samsum
-  #samsum
-  #samsum_simple_instruct
+  # samsum
+  samsum
+  samsum_simple_instruct
 
-  ## xsum
-  #xsum
-  #xsum_simple_instruct
+  # xsum
+  xsum
+  xsum_simple_instruct
 )
 
 ROOT_DIR=$(cd -- "$(dirname -- "${BASH_SOURCE[0]}")" &> /dev/null && pwd)
