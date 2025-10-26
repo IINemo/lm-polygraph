490e6a30ac01f455492c0dcba336d8c78539863f
Dr. Artem Shelmanov <ArtemShelmanov@gmail.com>
2025-10-26 16:38:11 +0400

    Add a huggingface-like wrapper (#402)
    * Add huggingface-like wrapper
    
    * Fix linter issues
    
    * Add test
    
    * Fix linter issue
    
    * Fix arguments
    
    * Reduce number of generated tokens in the test

--- src/lm_polygraph/__init__.py
+++ src/lm_polygraph/__init__.py
@@ -1,4 +1,5 @@
 from .utils.model import WhiteboxModel, BlackboxModel
 from .utils.manager import UEManager
 from .utils.estimate_uncertainty import estimate_uncertainty
+from .utils.causal_lm_with_uncertainty import CausalLMWithUncertainty
 from .utils.dataset import Dataset
