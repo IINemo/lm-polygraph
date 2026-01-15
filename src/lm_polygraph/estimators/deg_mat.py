66826954a3e51bb687c36a42b27f8cd808ae84f8
Roman Vashurin <rvashurin@gmail.com>
2026-01-15 22:40:06 +0400

    Add variations for semantic entropy, c_degmat measure

--- src/lm_polygraph/estimators/deg_mat.py
+++ src/lm_polygraph/estimators/deg_mat.py
@@ -89,3 +89,73 @@ class DegMat(Estimator):
                 log.debug(f"generated answers: {answers}")
             res.append(self.U_DegMat(i, stats))
         return np.array(res)
+
+
+class CDegMat(Estimator):
+    """
+    Estimates the sequence-level uncertainty of a language model following the method of
+    "The Degree Matrix" as provided in the paper https://arxiv.org/abs/2305.19187.
+    Works with both whitebox and blackbox models (initialized using
+    lm_polygraph.model_adapters.blackbox_model.BlackboxModel/WhiteboxModel).
+
+    Elements on diagonal of matrix D are sums of similarities between the particular number
+    (position in matrix) and other answers. Thus, it is an average pairwise distance
+    (lower values indicated smaller distance between answers which means greater uncertainty).
+    """
+
+    def __init__(
+        self,
+        similarity_score: Literal["NLI_score", "Jaccard_score"] = "NLI_score",
+        affinity: Literal["entail", "contra"] = "entail",  # relevant for NLI score case
+        verbose: bool = False,
+    ):
+        """
+        Parameters:
+            similarity_score (str): The argument to be processed. Possible values are:
+                - 'NLI_score': As a similarity score NLI score is used.
+                - 'Jaccard_score': As a similarity Jaccard score between responces is used.
+            affinity (str): The argument to be processed. Possible values are. Relevant for the case of NLI similarity score:
+                - 'entail': similarity(response_1, response_2) = p_entail(response_1, response_2)
+                - 'contra': similarity(response_1, response_2) = 1 - p_contra(response_1, response_2)
+        """
+        if similarity_score == "NLI_score":
+            if affinity == "entail":
+                super().__init__(["greedy_semantic_matrix_entail", "sample_texts"], "sequence")
+            else:
+                super().__init__(["greedy_semantic_matrix_contra", "sample_texts"], "sequence")
+        else:
+            raise ValueError("CDegMat only supports NLI_score as similarity_score.")
+
+        self.similarity_score = similarity_score
+        self.affinity = affinity
+        self.verbose = verbose
+
+    def __str__(self):
+        return f"CDegMat_{self.similarity_score}_{self.affinity}"
+
+    def C_DegMat(self, i, stats):
+        if self.affinity == "entail":
+            W = stats["greedy_semantic_matrix_entail"][i, :]
+        else:
+            W = 1 - stats["greedy_semantic_matrix_contra"][i, :]
+
+        return np.mean(W)
+
+    def __call__(self, stats: Dict[str, np.ndarray]) -> np.ndarray:
+        """
+        Estimates the uncertainties for each sample in the input statistics.
+
+        Parameters:
+            stats (Dict[str, np.ndarray]): input statistics, which for multiple samples includes:
+                * generated samples in 'sample_texts',
+                * matrix with semantic similarities in 'semantic_matrix_entail'/'semantic_matrix_contra'
+        Returns:
+            np.ndarray: float uncertainty for each sample in input statistics.
+                Higher values indicate more uncertain samples.
+        """
+        res = []
+        for i, answers in enumerate(stats["sample_texts"]):
+            if self.verbose:
+                log.debug(f"generated answers: {answers}")
+            res.append(self.C_DegMat(i, stats))
+        return np.array(res)
--- src/lm_polygraph/estimators/semantic_entropy.py
+++ src/lm_polygraph/estimators/semantic_entropy.py
@@ -17,7 +17,7 @@ class SemanticEntropy(Estimator):
     """
 
     def __init__(
-        self, verbose: bool = False, class_probability_estimation: str = "sum"
+        self, verbose: bool = False, class_probability_estimation: str = "sum", samples: str = "all", normalize: bool = False, estimator: str = "sample"
     ):
         self.class_probability_estimation = class_probability_estimation
         if self.class_probability_estimation == "sum":
@@ -31,12 +31,25 @@ class SemanticEntropy(Estimator):
 
         super().__init__(deps, "sequence")
         self.verbose = verbose
+        self.samples = samples
+        self.normalize = normalize
+        if estimator not in ["sample", "direct"]:
+            raise ValueError(
+                f"Unknown estimator: {estimator}. Use 'sample' or 'direct'."
+            )
+        self.estimator = estimator
 
     def __str__(self):
-        if self.class_probability_estimation == "sum":
-            return "SemanticEntropy"
-        elif self.class_probability_estimation == "frequency":
-            return "SemanticEntropyEmpirical"
+        base = "SemanticEntropy"
+        if self.samples == "unique":
+            base += "Unique"
+        if self.estimator == "direct":
+            base += "Direct"
+        if self.normalize:
+            base += "Normalized"
+        if self.class_probability_estimation == "frequency":
+            base += "Empirical"
+        return base
 
     def __call__(self, stats: Dict[str, np.ndarray]) -> np.ndarray:
         """
@@ -52,53 +65,107 @@ class SemanticEntropy(Estimator):
                 Higher values indicate more uncertain samples.
         """
         if self.class_probability_estimation == "sum":
-            loglikelihoods_list = stats["sample_log_probs"]
             hyps_list = stats["sample_texts"]
+            if self.normalize:
+                loglikelihoods_list = [[np.mean(ll) for ll in batch_ll] for batch_ll in stats["sample_log_likelihoods"]]
+                # Normalized likelihoods of sequences do not sum to 1, and may give class probabilities > 1 resulting in negative entropy.
+                # We don't know the partition function here, so we approximate it by normalizing over the unique samples in the batch. This estimator is consistent but biased.
+                indices = [np.unique(hyps, return_index=True)[1] for hyps in hyps_list]
+                loglikelihoods_list = [
+                    ll - np.logaddexp.reduce(np.array(ll)[ind])
+                    for ll, ind in zip(loglikelihoods_list, indices)
+                ]
+            else:
+                loglikelihoods_list = stats["sample_log_probs"]
+
         elif self.class_probability_estimation == "frequency":
-            loglikelihoods_list = None
             hyps_list = stats["sample_texts"]
+            loglikelihoods_list = None
+
+        index = []
+        for i, hyps in enumerate(hyps_list):
+            if self.samples == "unique":
+                _, ind = np.unique(hyps, return_index=True)
+                index.append(ind)
+            else:
+                index.append(list(range(0, len(hyps))))
+
+            #unique_hyps_list = []
+            #unique_ll_list = []
+
+            #for i, hyps in enumerate(hyps_list):
+                #hyps, index = np.unique(hyps, return_index=True)
+                #unique_hyps_list.append(hyps)
+                #if loglikelihoods_list is not None:
+                #    ll = np.array(loglikelihoods_list[i])[index]
+                #    unique_ll_list.append(ll)
+
+            #hyps_list = unique_hyps_list
+            #if loglikelihoods_list is not None:
+            #    loglikelihoods_list = unique_ll_list
 
         self._class_to_sample = stats["semantic_classes_entail"]["class_to_sample"]
         self._sample_to_class = stats["semantic_classes_entail"]["sample_to_class"]
 
-        return self.batched_call(hyps_list, loglikelihoods_list)
+        return self.batched_call(index, hyps_list, loglikelihoods_list)
 
     def batched_call(
         self,
+        index: List[List[int]],
         hyps_list: List[List[str]],
         loglikelihoods_list: Optional[List[List[float]]],
-        log_weights: Optional[List[List[float]]] = None,
     ) -> np.array:
-        if log_weights is None:
-            log_weights = [None for _ in hyps_list]
-
         semantic_logits = {}
         # Iteration over batch
         for i in range(len(hyps_list)):
+            ind = index[i]
+
+            hyps = np.array(hyps_list[i])
+
+            class_to_sample = [list(set(ind).intersection(set(cts))) for cts in self._class_to_sample[i]]
+            sample_to_class = {_id: self._sample_to_class[i][_id] for _id in ind}
+
             if self.class_probability_estimation == "sum":
+                ll = np.array(loglikelihoods_list[i])
                 class_likelihoods = [
-                    np.array(loglikelihoods_list[i])[np.array(class_idx)]
-                    for class_idx in self._class_to_sample[i]
+                    np.array(ll[np.array(class_idx)])
+                    for class_idx in class_to_sample
                 ]
                 class_lp = [
                     np.logaddexp.reduce(likelihoods)
                     for likelihoods in class_likelihoods
                 ]
             elif self.class_probability_estimation == "frequency":
-                num_samples = len(hyps_list[i])
+                num_samples = len(ind)
                 class_lp = np.log(
                     [
                         len(class_idx) / num_samples
-                        for class_idx in self._class_to_sample[i]
+                        for class_idx in class_to_sample
                     ]
                 )
 
-            if log_weights[i] is None:
-                log_weights[i] = [0 for _ in hyps_list[i]]
-            semantic_logits[i] = -np.mean(
-                [
-                    class_lp[self._sample_to_class[i][j]] * np.exp(log_weights[i][j])
-                    for j in range(len(hyps_list[i]))
-                ]
-            )
-        return np.array([semantic_logits[i] for i in range(len(hyps_list))])
+            if self.estimator == "direct":
+                semantic_logits[i] = -np.sum(
+                    [
+                        np.exp(class_lp[sample_to_class[j]]) * class_lp[sample_to_class[j]]
+                        for j in ind
+                    ]
+                )
+            else:
+                semantic_logits[i] = -np.mean(
+                    [
+                        class_lp[sample_to_class[j]]
+                        for j in ind
+                    ]
+                )
+
+        entropies = np.array([semantic_logits[i] for i in range(len(hyps_list))])
+        clipped_entropies = []
+
+        for ent in entropies:
+            if abs(ent) < 1e-8:
+                clipped_entropies.append(0.0)
+            else:
+                clipped_entropies.append(ent)
+
+        return np.array(clipped_entropies)
