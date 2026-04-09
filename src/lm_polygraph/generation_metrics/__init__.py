be539d5b2dd3bb13499ad0df68117ef31a534cb9
Vlad Smirnov <smirnovlad03@gmail.com>
2026-04-09 11:05:11 +0400

    Fix black formatting in generation_metrics __init__

--- src/lm_polygraph/generation_metrics/__init__.py
+++ src/lm_polygraph/generation_metrics/__init__.py
@@ -3,10 +3,12 @@ from .bleu import BLEUMetric
 from .model_score import ModelScoreSeqMetric, ModelScoreTokenwiseMetric
 from .bart_score import BartScoreSeqMetric
 from .accuracy import AccuracyMetric
+
 try:
     from .comet import Comet
 except ImportError:
     Comet = None
+
 from .alignscore import AlignScore
 from .openai_fact_check import OpenAIFactCheck
 from .bert_score import BertScoreMetric
